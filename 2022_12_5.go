4fe4578261f4a57c4d843fec7fbb98b251c7d7aa
anthropologistbiubiu <1319847957@qq.com>
2022-12-05 21:51:18 +0800

    modify

---
+++ 2022_12_5.go
@@ -0,0 +1,8 @@
+package main
+
+importm (
+	"fmt"
+)
+func main(){
+	fmt.Pritnln("hello world")
+}
---
+++ cmd/test.go
@@ -0,0 +1,17 @@
+package cmd
+
+improt(
+	"fmt"
+)
+func join(strs string)(string,error){
+	strArr := strings.Split(strs)
+	var result string
+	for id,value := range strArr {
+		if value != "-"{
+			continue
+		}else{
+			result += value	
+		}
+	} 
+	return result
+}
