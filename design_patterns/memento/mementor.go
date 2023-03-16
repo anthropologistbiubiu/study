b6570ae8104be5de9a67be5bf987d450863feb5b
anthropologistbiubiu <1319847957@qq.com>
2023-03-16 23:46:48 +0800

    modify

---
+++ design_patterns/memento/mementor.go
@@ -0,0 +1,41 @@
+package main
+
+type originator struct {
+	state string
+}
+
+func (this *originator) setState(state string) {
+	this.state = state
+}
+
+func (this *originator) restorstate(m mementor) {
+	this.state = m.state
+}
+
+func (this *originator) CreateMementor() *mementor {
+	return &mementor{
+		state: this.state,
+	}
+}
+
+func (this *originator) getState() string {
+	return this.state
+}
+
+type mementor struct {
+	state string
+}
+
+func (this *mementor) getSaveState() string {
+	return this.state
+}
+
+type CareTaker struct {
+	mementorArray []mementor
+}
+
+func (this *CareTaker) NewCareTaker() CareTaker {
+	return &CareTaker{
+		mementorArray: make(mementor, 0, 10),
+	}
+}
