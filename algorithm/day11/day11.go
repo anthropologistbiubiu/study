bd98cb9a1003d5fa24fb54b4236bbd147c3c59f9
anthropologistbiubiu <1319847957@qq.com>
2023-04-07 17:49:34 +0800

    Modify

--- algorithm/day11/day11.go
+++ algorithm/day11/day11.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 //给定一个 n x n 矩阵，其中每行和每列元素均按升序排序，找到矩阵中第 k 小的元素。
 //请注意，它是排序后的第 k 小元素，而不是第 k 个不同的元素。
 //
@@ -16,11 +18,20 @@ package main
 //
 //返回 13。
 //
-//
-//提示：
-//你可以假设 k 的值永远是有效的，1 ≤ k ≤ n2
-////进阶：
 func main() {
+	heap := &heap{
+		base: make([]int, 100),
+		size: 0,
+	}
+	heap.MinHeapInsert(heap, 9)
+	heap.MinHeapInsert(heap, 14)
+	heap.MinHeapInsert(heap, 5)
+	heap.MinHeapInsert(heap, 8)
+	heap.MinHeapInsert(heap, 0)
+	heap.MinHeapInsert(heap, 2)
+	arr := []int{9, 14, 5, 8, 0, 2}
+	heap.HeapSort(heap, arr, 6)
+	fmt.Println(arr)
 
 }
 
@@ -59,7 +70,6 @@ func (this *heap) HeapSort(pst *heap, ar []int, n int) {
 		AdjustDown(pst.base, 0, n)
 		curpos--
 	}
-	//排序
 	end := n - 1
 	for end > 0 {
 		Swap(&pst.base[0], &pst.base[end])
@@ -73,25 +83,22 @@ func (this *heap) HeapSort(pst *heap, ar []int, n int) {
 	}
 }
 
-func AdjustUp(base []int, start int) {
-	j := start       //j来接受pst->size也就是说 j的位置也就是插入节点的起始位置
-	i := (j - 1) / 2 //i来当做j的父节点
-	for j > 0 {      //开始向上调整，直到j没有父节点{
+func AdjutUp(base []int, start int) {
+	j := start
+	i := (j - 1)
+	for j > 0 {
 		if base[j] < base[i] {
 			Swap(&(base[i]), &(base[j]))
-			j = i           //j追到i的位置作为起始位置，即j以i父节点的位置作为起始调整位置，一步一个脚印向上调整
-			i = (j - 1) / 2 //重新定位父节点的位置
+			j = i
+			i = (j - 1) / 2
 		} else {
-			break //如果不需要调整，直接结束，开始下一轮的插入调整
+			break
 		}
 	}
 }
 
-func MinHeapInsert(php *heap, x int) {
-	if php.size < php.capacity {
-		php.base[php.size] = x
-		AdjustUp(php.base, php.size)
-		//调整为小堆结构，从下往上调整
-		php.size++ //开始下一轮的插入调整
-	}
+func (this *heap) MinHeapInsert(php *heap, x int) {
+	php.base[php.size] = x
+	//AdjustUp(php.base, php.size)
+	php.size++
 }
