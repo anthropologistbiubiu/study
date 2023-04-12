3ffb4361f2d9a132248437aa8fc76cf36d0b9bfb
anthropologistbiubiu <1319847957@qq.com>
2023-04-13 00:04:14 +0800

    modify

--- algorithm/day30/day30.go
+++ algorithm/day30/day30.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 /*
 给定两个数组，编写一个函数来计算它们的交集。
 
@@ -22,6 +24,35 @@ package main
 
 */
 
+func intersection(a []int, b []int) []int {
+	var length int
+	if len(a) > len(b) {
+		length = len(a)
+	} else {
+		length = len(b)
+	}
+	mp := make(map[int]int, length)
+	for _, v := range a {
+		mp[v] = 1
+	}
+	for _, v := range b {
+		if _, ok := mp[v]; ok {
+			mp[v]++
+		}
+	}
+	var ret []int
+	for k, v := range mp {
+		if v > 1 {
+			ret = append(ret, k)
+		}
+	}
+	return ret
+}
 func main() {
-
+	nums1 := []int{1, 2, 2, 1}
+	nums2 := []int{2, 2}
+	nums3 := []int{4, 9, 5}
+	nums4 := []int{9, 4, 9, 8, 4}
+	fmt.Println(intersection(nums1, nums2))
+	fmt.Println(intersection(nums3, nums4))
 }
