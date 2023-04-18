0dd79d4109bbad4d164e6043c546bef6eea228fb
anthropologistbiubiu <1319847957@qq.com>
2023-04-18 09:24:19 +0800

    Modify

--- algorithm/day37/day37.go
+++ algorithm/day37/day37.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 /*
 给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
 
@@ -24,25 +26,61 @@ package main
  *     Next *ListNode
  * }
  */
-type ListNode struct {
-	Val  int
-	Next *ListNode
-}
 
 func removeNthFromEnd(head *ListNode, k int) *ListNode {
 	var pre *ListNode = head
 	var next *ListNode = head
 	for i := 0; i < k; i++ {
-		next = next.Next
+		next = next.next
 	}
-	for next.Next != nil {
-		pre = pre.Next
-		next = next.Next
+	for next.next != nil {
+		pre = pre.next
+		next = next.next
 	}
-	pre.Next = pre.Next.Next
+	pre.next = pre.next.next
 	return head
+}
+
+func (this *List) ListTailInsert(x int) {
+	tem := NewListNode(x)
+	cur := this.l
+	if this.l == nil {
+		this.l = tem
+	} else {
+		for cur.next != nil {
+			cur = cur.next
+		}
+		cur.next = tem
+	}
+}
+
+func NewListNode(x int) *ListNode {
+	return &ListNode{
+		val:  x,
+		next: nil,
+	}
+}
+
+type ListNode struct {
+	val  int
+	next *ListNode
+}
+type List struct {
+	l *ListNode
 }
 
 func main() {
 
+	nums := []int{1, 2, 3, 4, 5}
+	l := &List{
+		l: nil,
+	}
+	for _, v := range nums {
+		l.ListTailInsert(v)
+	}
+	ret := removeNthFromEnd(l.l, 2)
+	for ret != nil {
+		fmt.Println(ret.val)
+		ret = ret.next
+	}
 }
