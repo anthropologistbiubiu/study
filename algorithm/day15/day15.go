fc64456e4cf0c42e1e6266fc90e498118f8dc45a
anthropologistbiubiu <1319847957@qq.com>
2023-04-22 21:24:43 +0800

    Modify

--- algorithm/day15/day15.go
+++ algorithm/day15/day15.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 /*
 给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
 
@@ -15,6 +17,34 @@ package main
 输出: false
 */
 
+func isPalindrome(s string) bool {
+
+	var top, tail = 0, len(s) - 1
+	for top <= tail {
+		for s[top] < 65 || s[top] > 90 && s[top] < 97 || s[top] > 122 {
+			top++
+		}
+		for s[tail] < 65 || s[tail] > 90 && s[tail] < 97 || s[tail] > 122 {
+			tail--
+		}
+		if s[top] != s[tail] && s[top] != s[tail]+32 && s[top]+32 != s[tail] {
+			return false
+		}
+		top++
+		tail--
+	}
+	return true
+}
 func main() {
+
+	//var s = "fghj , 8"
+	//fmt.Println(string(s[2]))
+	//fmt.Printf("%T\n", (s[2]))
+	//fmt.Println(string(104))
+	//fmt.Println(len(s))
+	var assist = "A man, a plan, a canal: Panama"
+	fmt.Println(isPalindrome(assist))
+	var scar = "race a car"
+	fmt.Println(isPalindrome(scar))
 
 }
