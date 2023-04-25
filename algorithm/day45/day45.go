8dc8a1cda7cda255fe2a2f730703945246a4b605
anthropologistbiubiu <1319847957@qq.com>
2023-04-25 20:39:43 +0800

    Modify

--- algorithm/day45/day45.go
+++ algorithm/day45/day45.go
@@ -1,3 +1,31 @@
 package main
 
-//
+func Insert(ch byte) {
+
+}
+
+func FirstAppearingOnce() byte {
+
+	return 1
+} //
+
+func permutation(s string) []string {
+	var res []string
+	for i, _ := range s {
+		dst := make([]string, len(res))
+		copy(dst, res)
+		for j, _ := range res {
+			res[j] = res[j] + string(s[i])
+		}
+		if len(res) == 0 {
+			res = append(res, string(s[i]))
+		} else {
+			for j, _ := range dst {
+				dst[j] = string(s[i]) + dst[j]
+			}
+			res = append(res, dst...)
+		}
+	}
+	return res
+
+}
