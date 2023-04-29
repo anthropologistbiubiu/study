7de7c1ca30613ad168ddfc3a369d129de08d2ce1
anthropologistbiubiu <1319847957@qq.com>
2023-04-29 15:38:53 +0800

    Modify

--- gRPC/day02/main.go
+++ gRPC/day02/main.go
@@ -1,16 +1,8 @@
 package main
 
-import (
-	"context"
-	"day02/pb"
-	"fmt"
-	"net"
-
-	"google.golang.org/grpc"
-)
-
 // hello server
 
+/*
 type server struct {
 	pb.UnimplementedGreeterServer
 }
@@ -19,6 +11,71 @@ func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloRe
 	return &pb.HelloResponse{Reply: "Hello " + in.Name}, nil
 }
 
+func (s *server) LotsOfReplies(in *pb.HelloRequest, stream pb.Greeter_LotsOfRepliesServer) error {
+	words := []string{
+		"你好",
+		"hello",
+		"こんにちは",
+		"안녕하세요",
+	}
+
+	for _, word := range words {
+		data := &pb.HelloResponse{
+			Reply: word + in.GetName(),
+		}
+		// 使用Send方法返回多个数据
+		if err := stream.Send(data); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// LotsOfGreetings 接收流式数据
+func (s *server) LotsOfGreetings(stream pb.Greeter_LotsOfGreetingsServer) error {
+	reply := "你好："
+	for {
+		// 接收客户端发来的流式数据
+		res, err := stream.Recv()
+		if err == io.EOF {
+			// 最终统一回复
+			return stream.SendAndClose(&pb.HelloResponse{
+				Reply: reply,
+			})
+		}
+		if err != nil {
+			return err
+		}
+		reply += res.GetName()
+	}
+}
+func (s *server) BidiHello(stream pb.Greeter_BidiHelloServer) error {
+	for {
+		// 接收流式请求
+		in, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+
+		reply := magic(in.GetName()) // 对收到的数据做些处理
+
+		// 返回流式响应
+		if err := stream.Send(&pb.HelloResponse{Reply: reply}); err != nil {
+			return err
+		}
+	}
+}
+func magic(s string) string {
+	s = strings.ReplaceAll(s, "吗", "")
+	s = strings.ReplaceAll(s, "吧", "")
+	s = strings.ReplaceAll(s, "你", "我")
+	s = strings.ReplaceAll(s, "？", "!")
+	s = strings.ReplaceAll(s, "?", "!")
+	return s
+}
 func main() {
 	// 监听本地的8972端口
 	lis, err := net.Listen("tcp", ":8972")
@@ -34,4 +91,7 @@ func main() {
 		fmt.Printf("failed to serve: %v", err)
 		return
 	}
+
 }
+
+*/
