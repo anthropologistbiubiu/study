b3bd1213d0ccddd9ceb2bba8e22241d8ea7ddcb4
anthropologistbiubiu <1319847957@qq.com>
2023-05-01 16:04:39 +0800

    Modify

--- goMicro/hello/main/main.go
+++ goMicro/hello/main/main.go
@@ -1,5 +1,6 @@
 package main
 
+/*
 import (
     "context"
     "github.com/asim/go-micro/plugins/registry/consul/v3"
@@ -8,27 +9,21 @@ import (
     proto "go-micro-test/proto"
     "log"
 )
-​
 type Greeter struct{}
-​
 func (g *Greeter) Hello(ctx context.Context, req *proto.Request, rsp *proto.Response) error {
     rsp.Greeting = "Hello " + req.Name
     return nil
 }
-​
 func main() {
     consulReg := consul.NewRegistry(registry.Addrs(":8500"))
-​
     service := micro.NewService(
         micro.Name("greeter"),
         micro.Registry(consulReg),
     )
-​
     service.Init()
-​
     proto.RegisterGreeterHandler(service.Server(), new(Greeter))
-​
     if err := service.Run(); err != nil {
         log.Fatal(err)
     }
 }
+*/
