473a0dc3be68d6fd17ac45ecd14ff3d9a99e224a
anthropologistbiubiu <1319847957@qq.com>
2023-05-03 17:28:56 +0800

    Modify

--- algorithm/day52/day52.go
+++ algorithm/day52/day52.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+//字符流中第一个不重复的字符
 
+var s string
+var set map[byte]int = make(map[byte]int)
+
+func Insert(ch byte) {
+
+	s += string(ch)
+	set[ch]++
+}
+
+func FirstAppearingOnce() byte {
+
+	for _, v := range []byte(s) {
+		if set[v] == 1 {
+			return v
+		}
+	}
+	return '#'
+}
 func main() {
-	fmt.Println("vim-go")
 }
