c4729977029f4517c3ff1423af7f202ea87ddaa4
anthropologistbiubiu <1319847957@qq.com>
2023-05-07 20:27:16 +0800

    Modify

---
+++ algorithm/day57/day57.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"golang.org/x/sync/semaphore"
+)
+
+const (
+	Limit  = 3 // 同時并行运行的goroutine上限
+	Weight = 1 // 每个goroutine获取信号量资源的权重
+)
+
+/*
+func main() {
+	urls := []string{
+		"http://www.example.com",
+		"http://www.example.net",
+		"http://www.example.net/foo",
+		"http://www.example.net/bar",
+		"http://www.example.net/baz",
+	}
+	s := semaphore.NewWeighted(Limit)
+	var w sync.WaitGroup
+	for _, u := range urls {
+		w.Add(1)
+		go func(u string) {
+			s.Acquire(context.Background(), Weight)
+			doSomething(u)
+			s.Release(Weight)
+			w.Done()
+		}(u)
+	}
+	w.Wait()
+
+	fmt.Println("All Done")
+}
+*/
+
+var s = semaphore.NewWeighted(10)
+var ctx context.Context
+
+func main() {
+
+	for i := 0; i < 1000; i++ {
+		err := s.Acquire(ctx, 1)
+		if err != nil {
+			fmt.Println(err)
+		}
+		go doSomething(i, s)
+	}
+}
+
+func doSomething(u int, s *semaphore.Weighted) { // 模拟抓取任务的执行
+	fmt.Println(u)
+	time.Sleep(1 * time.Second)
+	defer func() {
+		s.Release(1)
+	}()
+}
