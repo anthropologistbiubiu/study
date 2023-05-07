d624f54322ab239e06ab6e6805097d65bd6beb55
anthropologistbiubiu <1319847957@qq.com>
2023-05-07 13:30:38 +0800

    Modify

--- algorithm/day56/day56.go
+++ algorithm/day56/day56.go
@@ -1,5 +1,43 @@
 package main
 
+// 用一个栈空间和一个读写锁实现并发控制
+
+import (
+	"fmt"
+
+	"sync"
+
+	"time"
+)
+
 func main() {
+
+	var wg sync.WaitGroup
+
+	for i := 0; i < 5; i = i + 1 {
+
+		wg.Add(1)
+
+		go func(n int) {
+
+			// defer wg.Done()
+
+			defer wg.Add(-1)
+
+			EchoNumber(n)
+
+		}(i)
+
+	}
+
+	wg.Wait()
+
+}
+
+func EchoNumber(i int) {
+
+	time.Sleep(3e9)
+
+	fmt.Println(i)
 
 }
