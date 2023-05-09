45db97fe32115030b3fb48454688338527a5ce09
anthropologistbiubiu <1319847957@qq.com>
2023-05-09 21:14:20 +0800

    modify

--- sync/mutex/mutex.go
+++ sync/mutex/mutex.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"runtime"
+	"sync/atomic"
+)
 
+type Mutex struct {
+	key  int32
+	sema uint32
+}
+
+type Locker interface {
+	Lock()
+	Unlock()
+}
+
+func (m *Mutex) Lock() {
+	if atomic.AddInt32(&m.key, 1) == 1 {
+		return
+	}
+	runtime.Semacquire(&m.sema)
+}
+
+func (m *Mutex) Unlock() {
+	switch v := atomic.AddInt32(&m.key, -1); {
+	case v == 0:
+		return
+	case v == -1:
+		panic("sync: unlock of unlocked mutex")
+	}
+	runtime.Semrelease(&m.sema)
 }
