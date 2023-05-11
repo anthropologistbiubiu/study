ac9268e4ef65b9c6b49746a9583d15e5679c0699
anthropologistbiubiu <1319847957@qq.com>
2023-05-11 23:30:54 +0800

    Modify

--- golang_practice/02_test.go
+++ golang_practice/02_test.go
@@ -7,6 +7,9 @@ func Test_containerTest(t *testing.T) {
 		name string
 	}{
 		// TODO: Add test cases.
+		{"sun"},
+		{"wei"},
+		{"ming"}
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
