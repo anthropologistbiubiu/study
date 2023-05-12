008a6a0b68fc44ff78881fdce10432c6e1ec1195
anthropologistbiubiu <1319847957@qq.com>
2023-05-12 22:32:11 +0800

    Modify

--- signal/grace/day01.go
+++ signal/grace/day01.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
+// startHttpServer start http server
+func startHttpServer() {
+	mux := http.NewServeMux()
+	if err := http.ListenAndServe(":1608", mux); err != nil {
+		log.Fatal("startHttpServer ListenAndServe error: " + err.Error())
+	}
+}
+
 func main() {
 
 }
