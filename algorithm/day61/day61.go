e2a2f31aa181ff93c88c866a242de0d06654f0d6
anthropologistbiubiu <1319847957@qq.com>
2023-05-27 20:25:10 +0800

    Modify

--- algorithm/day61/day61.go
+++ algorithm/day61/day61.go
@@ -6,21 +6,48 @@ var ans = make([][]int, 0)
 
 func dfs(cur int, nums []int, curNum []int) {
 
+	//fmt.Println("ans", ans)
+
 	if cur == len(nums) {
-		ans = append(ans, curNum)
-		fmt.Println("ans", ans)
+		tmp := make([]int, len(curNum))
+		copy(tmp, curNum)
+		ans = append(ans, tmp)
 		return
 	}
 	for i := cur; i < len(nums); i++ {
 		curNum = append(curNum, nums[i])
 		dfs(i+1, nums, curNum)
-		fmt.Println("cur,i,curNum", cur, i, curNum)
 		curNum = curNum[:len(curNum)-1]
 		if len(curNum) == 0 {
 			continue
 		}
-		ans = append(ans, curNum)
-		fmt.Println("ans", ans)
+		if i == len(nums)-1 {
+			tmp := make([]int, len(curNum))
+			copy(tmp, curNum)
+			ans = append(ans, tmp)
+		}
+	}
+	/*
+		tmp := make([]int, len(curNum))
+		copy(tmp, curNum)
+		ans = append(ans, tmp)
+	*/
+}
+
+var (
+	path []int
+	res  [][]int
+)
+
+func dfs1(nums []int, start int) {
+	tmp := make([]int, len(path))
+	copy(tmp, path)
+	res = append(res, tmp)
+
+	for i := start; i < len(nums); i++ {
+		path = append(path, nums[i])
+		dfs1(nums, i+1)
+		path = path[:len(path)-1]
 	}
 }
 
@@ -28,14 +55,25 @@ func dfs(cur int, nums []int, curNum []int) {
 nums = [1,2,3]
 输出：[[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
 */
+
 func subsets(nums []int) [][]int {
 	var curNum = []int{}
 	dfs(0, nums, curNum)
 	return ans
 }
 
+var testArr = [][]int{}
+
+func test(nums []int) {
+	testArr = append(testArr, nums)
+	fmt.Println("testArr", testArr)
+}
+
 func main() {
-	nums := []int{1, 2, 3}
+	nums := []int{1, 2, 3, 4}
+	test(nums)
+	//nums[3] = 5
+	//fmt.Println(testArr)
 	subsets(nums)
 	fmt.Println(ans)
 }
