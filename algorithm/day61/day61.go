b7484b29e5f4ed144135591d7a024ff6d37b0e10
anthropologistbiubiu <1319847957@qq.com>
2023-05-27 14:00:37 +0800

    Modify

--- algorithm/day61/day61.go
+++ algorithm/day61/day61.go
@@ -2,26 +2,24 @@ package main
 
 import "fmt"
 
+var ans = make([][]int, 0)
+
+func dfs(cur int, nums []int, curNum []int) {
+
+	if cur == len(nums)-1 {
+		ans = append(ans, curNum)
+	}
+	curNum = append(curNum, nums[cur])
+	dfs(cur+1, nums, curNum)
+	ans = append(ans, curNum)
+	curNum = curNum[:len(curNum)-1]
+
+	dfs(cur, nums, curNum)
+}
+
 func subsets(nums []int) [][]int {
 
 	return [][]int{}
-}
-
-var ans = make([][]int, 0)
-
-var res = make([]int, 0)
-
-func dfs(cur int, nums []int) {
-
-	if cur == len(nums)-1 {
-		ans = append(ans, res)
-	}
-	dfs(cur+1, nums)
-	for cur < len(nums) {
-		cur++
-		dfs(cur, nums)
-	}
-
 }
 
 func main() {
