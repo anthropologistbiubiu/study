df65a0755dcd135bcc10e1c53cdfc3fefbd41383
anthropologistbiubiu <1319847957@qq.com>
2023-05-27 14:16:31 +0800

    Modify

--- algorithm/day61/day61.go
+++ algorithm/day61/day61.go
@@ -6,23 +6,32 @@ var ans = make([][]int, 0)
 
 func dfs(cur int, nums []int, curNum []int) {
 
-	if cur == len(nums)-1 {
+	if cur == len(nums) {
 		ans = append(ans, curNum)
+		fmt.Println(ans)
+		return
 	}
 	curNum = append(curNum, nums[cur])
 	dfs(cur+1, nums, curNum)
 	ans = append(ans, curNum)
 	curNum = curNum[:len(curNum)-1]
-
-	dfs(cur, nums, curNum)
+	cur++
+	dfs(cur+1, nums, curNum)
 }
 
+/*
+nums = [1,2,3]
+输出：[[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+*/
 func subsets(nums []int) [][]int {
 
-	return [][]int{}
+	var curNum = []int{}
+	dfs(0, nums, curNum)
+	return ans
 }
 
 func main() {
-	fmt.Println()
-
+	nums := []int{1, 2, 3}
+	subsets(nums)
+	fmt.Println(ans)
 }
