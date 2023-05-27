b52d3c45610c8eb6e370162dfa102aed2317d067
anthropologistbiubiu <1319847957@qq.com>
2023-05-27 13:30:14 +0800

    Modify

--- algorithm/day61/day61.go
+++ algorithm/day61/day61.go
@@ -2,9 +2,23 @@ package main
 
 import "fmt"
 
+func subsets(nums []int) [][]int {
+
+	return [][]int{}
+}
+
+var ans = make([][]int, 0)
+
+var res = make([]int, 0)
+
+func dfs(cur int, nums []int) {
+
+	if cur == len(nums)-1 {
+		ans = append(ans, res)
+	}
+	dfs(cur+1, nums)
+}
 func main() {
 	fmt.Println()
-	fmt.Println()
-	var a []string
-	a = append(a, "1")
+
 }
--- kratos/day01/day01.go
+++ kratos/day01/day01.go
@@ -33,7 +33,9 @@ func main() {
 		fmt.Println(err)
 	}
 	fmt.Printf("val:%+v type:%+T\n", val, val)
-	fmt.Println()
-	arr := make([]int, 0)
-	arr = append(arr, 1)
+	a, err := rdb.Get("").Result()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(a)
 }
