a9508146bc81a010912b8835f4a619f25c1102d6
anthropologistbiubiu <1319847957@qq.com>
2023-06-05 22:51:42 +0800

    modify

--- mongodb/day01/day01.go
+++ mongodb/day01/day01.go
@@ -1,12 +1,31 @@
 package main
 
-
-import(
+import (
+	"context"
 	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"time"
 )
 
-func main(){
+func main() {
+	var (
+		client     *mongo.Client
+		err        error
+		db         *mongo.Database
+		collection *mongo.Collection
+	)
+	//1.建立连接
+	client, err = mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:27017").SetConnectTimeout(5*time.Second))
+	if err != nil {
+		fmt.Print(err)
+		return
+	}
+	//2.选择数据库 my_db
+	db = client.Database("my_db")
 
-	fmt.Println()
-
+	//3.选择表 my_collection
+	collection = db.Collection("my_collection")
+	collection = collection
+	fmt.Println(collection)
 }
