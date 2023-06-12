8ab47c97c01e79d1a4cfcf2236477efb6f042bd1
anthropologistbiubiu <1319847957@qq.com>
2023-06-12 21:30:36 +0800

    modify

--- mongodb/day03/day03.go
+++ mongodb/day03/day03.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+)
+
 func main() {
+	clientOptions := options.Client().ApplyURI("mongodb://admin:password@localhost:27017")
+	var ctx = context.TODO()
+	// Connect to MongoDB
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Check the connection
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to MongoDB!")
+	defer client.Disconnect(ctx)
 
 }
