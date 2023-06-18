1f79f4c53c26cf57aae2184b293b830089297d9f
anthropologistbiubiu <1319847957@qq.com>
2023-06-18 22:28:18 +0800

    modify

---
+++ Shell/day12/day12.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+# 示例8：使用模式读取字符串列表
+# 在这里，“ /，/”模式用于根据逗号分割字符串值。
+# Define a list of string variable
+stringList=WordPress,Joomla,Magento
+
+# Use comma as separator and apply as pattern
+for val in ${stringList//,/ }
+do
+   echo $val
+done
