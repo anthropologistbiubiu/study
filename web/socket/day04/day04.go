68974c484b89fac525ba4e56385978a0f94a24cd
anthropologistbiubiu <1319847957@qq.com>
2023-06-27 00:24:59 +0800

    modify

--- web/socket/day04/day04.go
+++ web/socket/day04/day04.go
@@ -1,1 +1,36 @@
-package day04
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+)
+
+func client() {
+
+	conn, err := net.Dial("tcp", ":9091")
+	if err != nil {
+		fmt.Println(err)
+	}
+	for {
+		input := bufio.NewReader(os.Stdin)
+		var buff = []byte{}
+		_, err := input.Read(buff)
+		if err != nil {
+			fmt.Println(err)
+		}
+		conn.Write(buff)
+		recv := make([]byte, 0, 1024)
+		_, err = conn.Read(recv)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Printf("recive from remote server:%+v ,content is %+v\n", conn.RemoteAddr(), string(recv))
+	}
+
+}
+
+func main() {
+	client()
+}
