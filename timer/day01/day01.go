69c99b3a3bf595f2fe1a2de3ed4383907cbe8835
anthropologistbiubiu <1319847957@qq.com>
2023-07-10 21:34:48 +0800

    modidfy

--- timer/day01/day01.go
+++ timer/day01/day01.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"time"
 )
 
@@ -17,11 +18,45 @@ func WaitChannel(conn <-chan string) bool {
 	}
 }
 
+func DelayFunction() {
+	timer := time.NewTimer(5 * time.Second)
+	select {
+	case <-timer.C:
+		fmt.Println("Delayed 5s，...")
+	}
+}
+
+func AfterDemo() {
+	fmt.Println(time.Now().Second())
+	<-(time.After(1 * time.Second))
+	fmt.Println(time.Now().Second())
+}
+
+func AfterFuncDemo() {
+	fmt.Println("AfterFuncDemo start", time.Now().Format("20060102"))
+	time.AfterFunc(3*time.Second, func() {
+		fmt.Println("AfterFuncDemo end", time.Now().Format("20060102 13:14:15"))
+	})
+	fmt.Println("wait")
+	time.Sleep(4 * time.Second) //等待协程退出
+}
+
+func WorngTicker() {
+	for {
+		select {
+		case <- time.Tick(1 * time.Second)
+			log.Println("资源泄露")
+		}
+	}
+}
+
 func main() {
 
-	conn := make(chan string, 1)
+	//conn := make(chan string, 1)
 	//conn <- "sunweiming"
-	flag := WaitChannel(conn)
-	fmt.Println("flag", flag)
-
+	//flag := WaitChannel(conn)
+	//fmt.Println("flag", flag)
+	//DelayFunction()
+	//AfterDemo()
+	AfterFuncDemo()
 }
