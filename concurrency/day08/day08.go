f32287dfdef0d70534c1e79f6374304d5b58d9a7
anthropologistbiubiu <1319847957@qq.com>
2023-07-11 22:06:46 +0800

    modify

---
+++ concurrency/day08/day08.go
@@ -0,0 +1,37 @@
+package main
+
+import "sync"
+
+// 今天写一个两个协程交替打印的程序
+
+var bridge = make(chan int, 1)
+
+func doWork1() {
+	defer func() {
+		wg.Done()
+	}()
+	for i := 0; i < 10; i++ {
+		if i%2 != 0 {
+			println(i)
+			bridge <- i
+		}
+	}
+}
+
+func doWork2() {
+	defer wg.Done()
+	for {
+		i := <-bridge
+		println(i + 1)
+	}
+}
+
+var wg = sync.WaitGroup{}
+
+func main() {
+
+	wg.Add(2)
+	go doWork1()
+	go doWork2()
+	wg.Wait()
+}
