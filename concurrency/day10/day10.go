aded206269a22113c021f685232cf62cd4ad2ec3
anthropologistbiubiu <1319847957@qq.com>
2023-07-12 22:41:28 +0800

    modify

--- concurrency/day10/day10.go
+++ concurrency/day10/day10.go
@@ -1,16 +1,20 @@
 package main
 
-import "time"
+import (
+	"runtime"
+	"time"
+)
 
-var switchChan = make(chan struct{})
-var dataChan = make(chan int, 1)
+var chan1 = make(chan struct{})
+var chan2 = make(chan struct{})
 
 func work1() {
 
 	for i := 1; i <= 10; i++ {
 		if i%2 != 0 {
+			<-chan2
 			println("work1", i)
-			switchChan <- struct{}{}
+			chan1 <- struct{}{}
 		} else {
 			continue
 		}
@@ -22,8 +26,9 @@ func work2() {
 
 	for i := 1; i <= 10; i++ {
 		if i%2 == 0 {
+			<-chan1
 			println("work2", i)
-			<-switchChan
+			chan2 <- struct{}{}
 		} else {
 			continue
 		}
@@ -32,7 +37,12 @@ func work2() {
 
 func main() {
 
+	runtime.GOMAXPROCS(2)
+	go func() {
+		chan2 <- struct{}{}
+	}()
 	go work1()
 	go work2()
+
 	time.Sleep(5 * time.Second)
 }
