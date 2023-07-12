25b1953e9d0d593845f0c188b35324d0d060762a
anthropologistbiubiu <1319847957@qq.com>
2023-07-12 21:27:05 +0800

    modify

--- concurrency/day09/day09.go
+++ concurrency/day09/day09.go
@@ -1,8 +1,19 @@
-package mian
+package main
+
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 
 	var workChan = make(chan int)
-	<-workChan
+	go func() {
+		fmt.Println("gorutine start")
+		workChan <- 1
+		println("gorutine end")
+	}()
+	//<-workChan
 	println("end")
+	time.Sleep(2 * time.Second)
 }
