e74eaa22f4331588ed7a2389bcecac083265c2d9
anthropologistbiubiu <1319847957@qq.com>
2023-07-13 22:42:20 +0800

    modify

--- concurrency/day13/day13.go
+++ concurrency/day13/day13.go
@@ -1,21 +1,31 @@
 package main
 
+import "time"
+
 func main() {
 
 	var chan1 = make(chan int, 10)
+
 	for i := 1; i <= 10; i++ {
 		chan1 <- i
 	}
-	/*
-		for i := 1; i <= 10; i++ {
+
+	go func() {
+		for i := 1; i <= 11; i++ {
 			j := <-chan1
 			println(j)
 		}
-	*/
+	}()
 
-	for j := range chan1 {
-		println(j)
-	}
+	/*
+		go func() {
+			for j := range chan1 {
+				println(j)
+			}
+		}()
+	*/
+	// 哇神奇 ，在协程中处理管道 ，竟然没有报错
 	k := <-chan1
 	println("k", k)
+	time.Sleep(5 * time.Second)
 }
