eaf7780fe41240893f9a51d83085a843ba8e2c6f
anthropologistbiubiu <1319847957@qq.com>
2023-07-18 21:06:53 +0800

    modify

--- concurrency/day14/day14.go
+++ concurrency/day14/day14.go
@@ -1,5 +1,34 @@
+// 素数筛子
+// 一般写法
 package main
 
+import (
+	"fmt"
+)
+
+func generate(ch chan int) {
+	for i := 2; i < 100; i++ {
+		ch <- i
+	}
+}
+
+func filter(in, out chan int, prime int) {
+	for {
+		i := <-in
+		if i%prime != 0 {
+			out <- i
+		}
+	}
+}
+
 func main() {
-	println("hello world")
+	ch := make(chan int)
+	go generate(ch)
+	for {
+		prime := <-ch
+		fmt.Print(prime, " ")
+		ch1 := make(chan int)
+		go filter(ch, ch1, prime)
+		ch = ch1
+	}
 }
