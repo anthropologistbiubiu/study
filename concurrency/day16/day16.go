3d33a92cad411c0b81c61c8a0a54aa31b9729cef
anthropologistbiubiu <1319847957@qq.com>
2023-07-19 23:29:07 +0800

    modify

---
+++ concurrency/day16/day16.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func filter(in, out chan int, prime int) {
+	for {
+		i := <-in
+		if i%prime != 0 {
+			out <- i
+		}
+	}
+}
+func counter(c chan int) {
+	i := 2
+	for {
+		c <- i
+		i++
+	}
+}
+func main() {
+	c := make(chan int)
+	go counter(c)
+
+	for i := 0; i < 11; i++ {
+		p := <-c
+		fmt.Println(p)
+		primes := make(chan int)
+		go filter(c, primes, p)
+		c = primes
+	}
+}
