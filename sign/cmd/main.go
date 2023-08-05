5c73655f801edb6c6c608c037c88f7edf1a0ec89
anthropologistbiubiu <1319847957@qq.com>
2023-08-05 15:36:10 +0800

    modify

--- sign/cmd/main.go
+++ sign/cmd/main.go
@@ -6,13 +6,16 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
+	clientv3 "go.etcd.io/etcd/client/v3"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/resolver"
 	"log"
 	"net"
 	"os"
 	"os/signal"
 	"sign/proto"
+	"strings"
 	"sync"
 	"syscall"
 	"time"
@@ -103,5 +106,130 @@ func main() {
 	// 在服务器退出时，注销服务
 	if err := deregisterService(serviceAddr); err != nil {
 		log.Fatalf("Failed to deregister service: %v", err)
+	}
+}
+
+func registerService(serviceAddr string) error {
+	etcdCli, err := clientv3.New(clientv3.Config{
+		Endpoints: etcdEndpoints,
+	})
+	if err != nil {
+		return err
+	}
+	defer etcdCli.Close()
+
+	key := etcdKeyPrefix + serviceAddr
+	_, err = etcdCli.Put(context.Background(), key, serviceAddr)
+	return err
+}
+func deregisterService(serviceAddr string) error {
+	etcdCli, err := clientv3.New(clientv3.Config{
+		Endpoints: etcdEndpoints,
+	})
+	if err != nil {
+		return err
+	}
+	defer etcdCli.Close()
+
+	key := etcdKeyPrefix + serviceAddr
+	_, err = etcdCli.Delete(context.Background(), key)
+	return err
+}
+func init() {
+	// 注册etcd的服务发现解析器
+	resolver.Register(&etcdResolverBuilder{})
+}
+
+type etcdResolverBuilder struct{}
+
+func (*etcdResolverBuilder) Build(target resolver.Target, cc resolver.ClientConn, opts resolver.BuildOptions) (resolver.Resolver, error) {
+	r := &etcdResolver{
+		target: target,
+		cc:     cc,
+	}
+	go r.watch()
+	return r, nil
+}
+func (*etcdResolverBuilder) Scheme() string {
+	return "etcd"
+}
+
+type etcdResolver struct {
+	target resolver.Target
+	cc     resolver.ClientConn
+}
+
+func (r *etcdResolver) ResolveNow(options resolver.ResolveNowOptions) {}
+
+func (r *etcdResolver) Close() {}
+func (r *etcdResolver) watch() {
+	etcdCli, err := clientv3.New(clientv3.Config{
+		Endpoints: etcdEndpoints,
+	})
+	if err != nil {
+		log.Fatalf("Failed to create etcd client: %v", err)
+	}
+	defer etcdCli.Close()
+
+	key := etcdKeyPrefix + r.target.Endpoint
+	for {
+		resp, err := etcdCli.Get(context.Background(), key)
+		if err != nil {
+			log.Printf("Failed to get etcd key %s: %v", key, err)
+			time.Sleep(time.Second)
+			continue
+		}
+
+		var addresses []resolver.Address
+		for _, kv := range resp.Kvs {
+			addresses = append(addresses, resolver.Address{Addr: strings.TrimPrefix(string(kv.Value), "http://")})
+		}
+
+		r.cc.NewAddress(addresses)
+		// 等待下一次etcd变更通知
+		r.watchKey(key, resp.Header.Revision+1)
+	}
+}
+
+func (r *etcdResolver) watchKey(key string, revision int64) {
+	etcdCli, err := clientv3.New(clientv3.Config{
+		Endpoints: etcdEndpoints,
+	})
+	if err != nil {
+		log.Fatalf("Failed to create etcd client: %v", err)
+	}
+	defer etcdCli.Close()
+
+	rch := etcdCli.Watch(context.Background(), key, clientv3.WithRev(revision))
+	for wresp := range rch {
+		for _, ev := range wresp.Events {
+			switch ev.Type {
+			case clientv3.EventTypePut:
+				r.cc.NewAddress([]resolver.Address{{Addr: strings.TrimPrefix(string(ev.Kv.Value), "http://")}})
+			case clientv3.EventTypeDelete:
+				r.cc.NewAddress([]resolver.Address{})
+			}
+		}
+	}
+}
+func (r *etcdResolver) watchKey(key string, revision int64) {
+	etcdCli, err := clientv3.New(clientv3.Config{
+		Endpoints: etcdEndpoints,
+	})
+	if err != nil {
+		log.Fatalf("Failed to create etcd client: %v", err)
+	}
+	defer etcdCli.Close()
+
+	rch := etcdCli.Watch(context.Background(), key, clientv3.WithRev(revision))
+	for wresp := range rch {
+		for _, ev := range wresp.Events {
+			switch ev.Type {
+			case clientv3.EventTypePut:
+				r.cc.NewAddress([]resolver.Address{{Addr: strings.TrimPrefix(string(ev.Kv.Value), "http://")}})
+			case clientv3.EventTypeDelete:
+				r.cc.NewAddress([]resolver.Address{})
+			}
+		}
 	}
 }
