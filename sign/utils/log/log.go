44bad13b5646e61a8424d4efe99f64e024ade82c
anthropologistbiubiu <1319847957@qq.com>
2023-08-09 23:57:24 +0800

    modify

--- sign/utils/log/log.go
+++ sign/utils/log/log.go
@@ -45,3 +45,55 @@ func New(out io.Writer, level Level) *Logger {
 	)
 	return &Logger{l: zap.New(core), al: &al}
 }
+
+func (l *Logger) SetLevel(level Level) {
+	if l.al != nil {
+		l.al.SetLevel(level)
+	}
+}
+
+type Field = zap.Field
+
+func (l *Logger) Debug(msg string, fields ...Field) {
+	l.l.Debug(msg, fields...)
+}
+
+func (l *Logger) Info(msg string, fields ...Field) {
+	l.l.Info(msg, fields...)
+}
+
+func (l *Logger) Warn(msg string, fields ...Field) {
+	l.l.Warn(msg, fields...)
+}
+
+func (l *Logger) Error(msg string, fields ...Field) {
+	l.l.Error(msg, fields...)
+}
+
+func (l *Logger) Panic(msg string, fields ...Field) {
+	l.l.Panic(msg, fields...)
+}
+
+func (l *Logger) Fatal(msg string, fields ...Field) {
+	l.l.Fatal(msg, fields...)
+}
+
+func (l *Logger) Sync() error {
+	return l.l.Sync()
+}
+
+var std = New(os.Stderr, InfoLevel)
+
+func Default() *Logger         { return std }
+func ReplaceDefault(l *Logger) { std = l }
+
+func SetLevel(level Level) { std.SetLevel(level) }
+
+func Debug(msg string, fields ...Field) { std.Debug(msg, fields...) }
+func Info(msg string, fields ...Field)  { std.Info(msg, fields...) }
+func Warn(msg string, fields ...Field)  { std.Warn(msg, fields...) }
+func Error(msg string, fields ...Field) { std.Error(msg, fields...) }
+func Panic(msg string, fields ...Field) { std.Panic(msg, fields...) }
+func Fatal(msg string, fields ...Field) { std.Fatal(msg, fields...) }
+
+func Sync() error { return std.Sync() }
