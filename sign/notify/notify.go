dfeeca5850a08f145f18e29b86f60d7e840c9e68
anthropologistbiubiu <1319847957@qq.com>
2023-08-16 23:47:57 +0800

    modify

--- sign/notify/notify.go
+++ sign/notify/notify.go
@@ -1,9 +1,78 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/Shopify/sarama"
+	"os"
+	"os/signal"
+	"sync"
+)
 
-	// 这里主要是一个kakfa消费者服务 实习对回调请求的异步处理
+func consumer() {
+	var wg sync.WaitGroup
+	consumer, err := sarama.NewConsumer([]string{"172.20.3.13:30901"}, nil)
+	if err != nil {
+		fmt.Println("Failed to start consumer: %s", err)
+		return
+	}
+	partitionList, err := consumer.Partitions("test0") //获得该topic所有的分区
+	if err != nil {
+		fmt.Println("Failed to get the list of partition:, ", err)
+		return
+	}
 
+	for partition := range partitionList {
+		pc, err := consumer.ConsumePartition("test0", int32(partition), sarama.OffsetNewest)
+		if err != nil {
+			fmt.Println("Failed to start consumer for partition %d: %s\n", partition, err)
+			return
+		}
+		wg.Add(1)
+		go func(sarama.PartitionConsumer) { //为每个分区开一个go协程去取值
+			for msg := range pc.Messages() { //阻塞直到有值发送过来，然后再继续等待
+				fmt.Printf("Partition:%d, Offset:%d, key:%s, value:%s\n", msg.Partition, msg.Offset, string(msg.Key), string(msg.Value))
+			}
+			defer pc.AsyncClose()
+			wg.Done()
+		}(pc)
+	}
+	wg.Wait()
+}
+func main1() {
+	consumer()
 }
 
+func main() {
+	config := sarama.NewConfig()
+	config.Consumer.Return.Errors = true
 
+	brokers := []string{"127.0.0.1:9092"} // Kafka brokers
+
+	consumer, err := sarama.NewConsumer(brokers, config)
+	if err != nil {
+		fmt.Println("errrrr", err)
+	}
+	defer consumer.Close()
+
+	topic := "test-topic"
+	partitionConsumer, err := consumer.ConsumePartition(topic, 0, sarama.OffsetNewest)
+	if err != nil {
+		fmt.Println("NNNNNNNNNNN", err)
+	}
+	defer partitionConsumer.Close()
+
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, os.Interrupt)
+
+ConsumerLoop:
+	for {
+		select {
+		case msg := <-partitionConsumer.Messages():
+			fmt.Printf("Received message: %s\n", string(msg.Value))
+		case err := <-partitionConsumer.Errors():
+			fmt.Println("Error:", err.Err)
+		case <-signals:
+			break ConsumerLoop
+		}
+	}
+}
--- sign/paysrv/main.go
+++ sign/paysrv/main.go
@@ -1,9 +1,14 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"github.com/Shopify/sarama"
+	"github.com/gin-gonic/gin"
+	"log"
+)
 
 // 这里主要是暴露一个服务
-func main() {
+func main1() {
 	r := gin.New()
 	r.POST("/payment/create")
 	r.POST("/payment/info")
@@ -12,6 +17,43 @@ func main() {
 	r.POST("/refound/create")
 	r.POST("/refound/info")
 	r.POST("/payment/notify")
-	r.POST("/cashout/notify")
+	r.POST("/cashout/notify") // 将回调用请求转发到对应的服务
 	r.Run(":5555")
+}
+
+func main() {
+	// Kafka 服务器地址
+	brokerList := []string{"localhost:9092"}
+
+	// 创建配置
+	config := sarama.NewConfig()
+	config.Producer.RequiredAcks = sarama.WaitForAll
+	config.Producer.Partitioner = sarama.NewRandomPartitioner
+	config.Producer.Return.Successes = true
+	config.Producer.Return.Errors = true
+
+	// 创建生产者
+	producer, err := sarama.NewSyncProducer(brokerList, config)
+	if err != nil {
+		log.Fatalln("Failed to start Sarama producer:", err)
+	}
+	defer producer.Close()
+
+	// 主题名称
+	topic := "test-topic"
+
+	// 要发送的消息
+	message := &sarama.ProducerMessage{
+		Topic: topic,
+		Key:   sarama.StringEncoder("key"),
+		Value: sarama.StringEncoder("Hello, Kafka!"),
+	}
+
+	// 发送消息
+	partition, offset, err := producer.SendMessage(message)
+	if err != nil {
+		log.Printf("Failed to send message: %v\n", err)
+	} else {
+		fmt.Printf("Message sent to partition %d at offset %d\n", partition, offset)
+	}
 }
