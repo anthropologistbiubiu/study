c3007f4e6c625cdacc54ab86271746ee312700d4
anthropologistbiubiu <1319847957@qq.com>
2023-08-16 22:04:35 +0800

    modify

---
+++ sign/notify/notify.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+
+	// 这里主要是一个kakfa消费者服务 实习对回调请求的异步处理
+
+}
+
+
--- sign/paysrv/main.go
+++ sign/paysrv/main.go
@@ -2,8 +2,8 @@ package main
 
 import "github.com/gin-gonic/gin"
 
+// 这里主要是暴露一个服务
 func main() {
-
 	r := gin.New()
 	r.POST("/payment/create")
 	r.POST("/payment/info")
@@ -11,6 +11,7 @@ func main() {
 	r.POST("/cashout/info")
 	r.POST("/refound/create")
 	r.POST("/refound/info")
+	r.POST("/payment/notify")
+	r.POST("/cashout/notify")
 	r.Run(":5555")
-
 }
