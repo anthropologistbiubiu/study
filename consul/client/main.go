2e68777ff19f9105e91e73799439c2daca6b75b4
anthropologistbiubiu <1319847957@qq.com>
2023-08-26 13:08:25 +0800

    modify

--- consul/client/main.go
+++ consul/client/main.go
@@ -6,9 +6,7 @@ import (
 	"fmt"
 	"github.com/hashicorp/consul/api"
 	"google.golang.org/grpc"
-	"math/rand"
 	"strconv"
-	"time"
 )
 
 func http_main() {
@@ -48,37 +46,40 @@ func main() {
 		return
 	}
 	serviceName := "jobservice"
+	// 定义一个grpc 负载均衡器
 
 	// 创建一个新的Catalog服务查询实例
-	catalog := cc.Catalog()
+	/*
+		catalog := cc.Catalog()
 
-	// 查询指定服务的实例
-	serviceEntries, _, err := catalog.Service(serviceName, "", nil)
-	if err != nil {
-		fmt.Println("UUU", err)
-	}
-	// 打印服务实例的信息
-	for _, entry := range serviceEntries {
-		fmt.Printf("Service: %s, Address: %s, Port: %d\n", entry.ServiceName, entry.Address, entry.ServicePort)
-	}
-	serviceMap, err := cc.Agent().ServicesWithFilter("Service==`jobservice`")
-	fmt.Println("WWWWWWWWWWC", serviceMap)
-	if err != nil {
-		fmt.Printf("query service from consul failed, err:%v\n", err)
-		return
-	}
-	// 选一个服务机（这里选最后一个）
-	var addr string
-	adds := []string{}
-	for k, v := range serviceMap {
-		fmt.Printf("%s:%#v\n", k, v)
-		addr = v.Address + ":" + strconv.Itoa(v.Port)
-		adds = append(adds, addr)
-	}
+		// 查询指定服务的实例
+		serviceEntries, _, err := catalog.Service(serviceName, "", nil)
+		if err != nil {
+			fmt.Println("UUU", err)
+		}
+		// 打印服务实例的信息
+		for _, entry := range serviceEntries {
+			fmt.Printf("Service: %s, Address: %s, Port: %d\n", entry.ServiceName, entry.Address, entry.ServicePort)
+		}
+		serviceMap, err := cc.Agent().ServicesWithFilter("Service==`jobservice`")
+		fmt.Println("WWWWWWWWWWC", serviceMap)
+		if err != nil {
+			fmt.Printf("query service from consul failed, err:%v\n", err)
+			return
+		}
+		// 选一个服务机（这里选最后一个）
+		var addr string
+		adds := []string{}
+		for k, v := range serviceMap {
+			fmt.Printf("%s:%#v\n", k, v)
+			addr = v.Address + ":" + strconv.Itoa(v.Port)
+			adds = append(adds, addr)
+		}
 
-	rand.Seed(time.Now().UnixMilli())
-	index := rand.Intn(100) % len(adds)
-	addr = adds[index]
+		rand.Seed(time.Now().UnixMilli())
+		index := rand.Intn(100) % len(adds)
+		addr = adds[index]
+	*/
 	conn, err := grpc.Dial(addr, grpc.WithInsecure())
 	if err != nil {
 		fmt.Println("failed to connect: %v", err)
