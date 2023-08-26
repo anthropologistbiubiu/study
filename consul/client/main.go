a98fad706e86cee07d66c8c1b762bc6f2e52da49
anthropologistbiubiu <1319847957@qq.com>
2023-08-26 17:38:51 +0800

    modify

--- consul/client/main.go
+++ consul/client/main.go
@@ -6,8 +6,11 @@ import (
 	"fmt"
 	"github.com/hashicorp/consul/api"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/keepalive"
+	"google.golang.org/grpc/resolver"
 	"strconv"
+	"sync"
+	"time"
 )
 
 func http_main() {
@@ -39,20 +42,100 @@ func http_main() {
 	fmt.Println("result", addr)
 }
 
-func main() {
+const consulScheme = "consul"
 
-	cc, err := api.NewClient(api.DefaultConfig())
+type consulResolverBuilder struct{}
+
+func (b *consulResolverBuilder) Build(target resolver.Target, cc resolver.ClientConn, opts resolver.BuildOptions) (resolver.Resolver, error) {
+	r := &consulResolver{
+		target: target,
+		cc:     cc,
+	}
+	r.start()
+	return r, nil
+}
+
+func (*consulResolverBuilder) Scheme() string {
+	return "consul"
+}
+
+type consulResolver struct {
+	target resolver.Target
+	cc     resolver.ClientConn
+	mu     sync.Mutex
+}
+
+func (r *consulResolver) start() {
+	// 解析服务名
+	serviceName := r.target.Endpoint()
+	fmt.Printf("开始解析服务 ：%+v\n", serviceName)
+
+	// 初始化 Consul 客户端
+	consulClient, err := api.NewClient(api.DefaultConfig())
 	if err != nil {
-		fmt.Printf("api.NewClient failed, err:%v\n", err)
+		fmt.Printf("创建 Consul 客户端失败：%v\n", err)
 		return
 	}
-	serviceName := "jobservice"
+	/*
+		serviceEntries, _, err := catalog.Service(serviceName, "", nil)
+		if err != nil {
+			fmt.Println("UUU", err)
+		}
+		// 打印服务实例的信息
+		for _, entry := range serviceEntries {
+			fmt.Printf("Service: %s, Address: %s, Port: %d\n", entry.ServiceName, entry.Address, entry.ServicePort)
+		}
+	*/
+	// 查询服务实例
+	instances, _, err := consulClient.Catalog().Service(serviceName, "", nil)
+	if err != nil {
+		fmt.Printf("查询服务实例失败：%v\n", err)
+		return
+	}
+	// 构建解析器状态
+	var addresses []resolver.Address
+	for _, instance := range instances {
+		addresses = append(addresses, resolver.Address{
+			Addr: fmt.Sprintf("%s:%d", instance.ServiceAddress, instance.ServicePort),
+		})
+	}
+	// 更新解析器状态
+	fmt.Println("wwwww 查看解析出来的地址", addresses)
+	r.cc.UpdateState(resolver.State{
+		Addresses: addresses,
+	})
+
+}
+func (r *consulResolver) ResolveNow(o resolver.ResolveNowOptions) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	// 在此处可以实现主动刷新服务实例列表
+	r.cc.UpdateState(resolver.State{Addresses: []resolver.Address{{Addr: "localhost:50051"}}})
+}
+
+func (r *consulResolver) Close() {
+	// 关闭资源
+}
+
+func (r *consulResolver) Scheme() string {
+	return consulScheme
+}
+
+func (r *consulResolver) watch() {
+	// 在此处可以监听 Consul 服务变化，并更新服务实例列表
+}
+func main() {
+
+	/*
+		cc, err := api.NewClient(api.DefaultConfig())
+		if err != nil {
+			fmt.Printf("api.NewClient failed, err:%v\n", err)
+			return
+		}
+	*/
+	//serviceName := "jobservice"
 	// consul服务
-	"consul://127.0.0.1:8500/hello?healthy=true",
-	// 指定round_robin策略
-	grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy": "round_robin"}`),
-	grpc.WithTransportCredentials(insecure.NewCredentials()),
-	// 定义一个grpc 负载均衡器
+	//address := "consul://127.0.0.1:8500/jobservice"
 
 	// 创建一个新的Catalog服务查询实例
 	/*
@@ -86,7 +169,19 @@ func main() {
 		index := rand.Intn(100) % len(adds)
 		addr = adds[index]
 	*/
-	conn, err := grpc.Dial(addr, grpc.WithInsecure())
+	resolver.Register(&consulResolverBuilder{})
+
+	conn, err := grpc.DialContext(
+		context.Background(),
+		fmt.Sprintf("%s:///%s", consulScheme, "jobservice"),                     // gRPC 服务名
+		grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy": "round_robin"}`), // 使用轮询负载均衡
+		grpc.WithInsecure(),
+		grpc.WithKeepaliveParams(keepalive.ClientParameters{
+			Time:    30 * time.Second,
+			Timeout: 10 * time.Second,
+		}),
+	)
+	//conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy": "round_robin"}`))
 	if err != nil {
 		fmt.Println("failed to connect: %v", err)
 	}
