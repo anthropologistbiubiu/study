9f2b8f6e17eca9a3a0b21c013d2cd96ad95fc401
anthropologistbiubiu <1319847957@qq.com>
2023-08-27 14:32:29 +0800

    modify

--- consul/utils/limiter/token_bucket.go
+++ consul/utils/limiter/token_bucket.go
@@ -9,9 +9,9 @@ import (
 
 // 在这里定义一个全局的token bucket 限流算法 实现对服务的限流保护
 
-func main() {
+func test_tokenBucket() {
 	// 创建一个令牌桶，每秒产生5个令牌
-	limiter := rate.NewLimiter(5, 1)
+	limiter := rate.NewLimiter(5, 3)
 	// 模拟一些请求
 	for i := 0; i < 10; i++ {
 		if limiter.Allow() {
@@ -19,14 +19,14 @@ func main() {
 		} else {
 			fmt.Printf("Request %d dropped\n", i+1)
 		}
-		time.Sleep(100 * time.Millisecond) // 模拟请求间隔
+		time.Sleep(500 * time.Millisecond) // 模拟请求间隔
 	}
 	for i := 0; i < 4; i++ {
 		if limiter.Allow() {
 			fmt.Printf("abnormal Request %d processed\n", i+1)
 		} else {
 			if err := limiter.Wait(context.Background()); err == nil {
-				fmt.Printf("abnormal Request %d processed\n", i+1)
+				fmt.Printf("abnormal block Request %d processed\n", i+1)
 			} else {
 				fmt.Printf("abnormal Request %d dropped err:%+v \n", i+1, err)
 			}
