777b44efde32a8398b8e8e8f99cb264f8f7a1293
anthropologistbiubiu <1319847957@qq.com>
2023-09-09 20:56:24 +0800

    modify

--- wire/main/wire.go
+++ wire/main/wire.go
@@ -1,8 +1,9 @@
 //go:build wireinject
 // +build wireinject
 
-// The build tag makes sure the stub is not built in the final build.
 package main
+
+// The build tag makes sure the stub is not built in the final build.
 
 import "github.com/google/wire"
 
