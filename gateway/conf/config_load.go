4c7ab944a090d26ddc7357454842cde13fe62237
anthropologistbiubiu <1319847957@qq.com>
2023-09-21 22:07:33 +0800

    modify

--- gateway/conf/config_load.go
+++ gateway/conf/config_load.go
@@ -1,6 +1,23 @@
 package conf
 
-// 可以在这里定义结构体完成初始化
-func init() {
+import (
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+)
 
+// 可以在这里定义结构体完成初始化
+type Configs struct {
+}
+
+var ConfigData = &Configs{}
+
+func init() {
+	configData, err := ioutil.ReadFile("../cinfig/config.yaml")
+	if err != nil {
+		panic(err)
+	}
+	// 解析 YAML 配置并序列化到配置结构体
+	if err := yaml.Unmarshal(configData, ConfigData); err != nil {
+		panic(err)
+	}
 }
