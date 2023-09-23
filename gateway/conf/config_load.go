56f04739d8198c6fbae0e1e38db30a80a658be12
anthropologistbiubiu <1319847957@qq.com>
2023-09-23 17:42:26 +0800

    modify

--- gateway/conf/config_load.go
+++ gateway/conf/config_load.go
@@ -2,7 +2,8 @@ package conf
 
 import (
 	"fmt"
-	"gopkg.in/yaml.v2"
+	protos "gateway/conf"
+	"github.com/golang/protobuf/proto"
 	"io/ioutil"
 )
 
@@ -11,6 +12,7 @@ type Configs struct {
 }
 
 var ConfigData = &Configs{}
+var server = protos.Server{}
 
 func LoadConfig() {
 	configData, err := ioutil.ReadFile("./config/config.yaml")
@@ -18,8 +20,10 @@ func LoadConfig() {
 		panic(err)
 	}
 	// 解析 YAML 配置并序列化到配置结构体
-	if err := yaml.Unmarshal(configData, ConfigData); err != nil {
+	if err := proto.UnmarshalText(string(configData), server); err != nil {
 		panic(err)
 	}
+	// 现在我把这个配置怎么序列化位对应的结构体，使用的protobuf 协议
+	// 然后使用这个服务封装 es 和 mysql 的交互
 	fmt.Printf("%v \n", ConfigData)
 }
