c5ee7bd5eff0181a22934098f42b5643f8bdc33a
anthropologistbiubiu <1319847957@qq.com>
2023-09-24 09:36:09 +0800

    modify

--- gateway/conf/config_load.go
+++ gateway/conf/config_load.go
@@ -2,7 +2,6 @@ package conf
 
 import (
 	"fmt"
-	protos "gateway/conf"
 	"github.com/golang/protobuf/proto"
 	"io/ioutil"
 )
@@ -12,7 +11,7 @@ type Configs struct {
 }
 
 var ConfigData = &Configs{}
-var server = protos.Server{}
+var server = &Server{}
 
 func LoadConfig() {
 	configData, err := ioutil.ReadFile("./config/config.yaml")
@@ -20,10 +19,11 @@ func LoadConfig() {
 		panic(err)
 	}
 	// 解析 YAML 配置并序列化到配置结构体
-	if err := proto.UnmarshalText(string(configData), server); err != nil {
-		panic(err)
+	if err := proto.Unmarshal(configData, server); err != nil {
+		fmt.Println("UUU", err)
 	}
 	// 现在我把这个配置怎么序列化位对应的结构体，使用的protobuf 协议
 	// 然后使用这个服务封装 es 和 mysql 的交互
-	fmt.Printf("%v \n", ConfigData)
+	fmt.Println("server.Address", server.Address)
+	fmt.Println("server.Address", server.Port)
 }
--- gateway/go.mod
+++ gateway/go.mod
@@ -4,18 +4,17 @@ go 1.19
 
 require (
 	github.com/go-kratos/kratos/v2 v2.7.0
+	github.com/golang/protobuf v1.5.3
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.18.0
 	golang.org/x/net v0.15.0
 	google.golang.org/genproto/googleapis/api v0.0.0-20230913181813-007df8e322eb
 	google.golang.org/grpc v1.58.1
 	google.golang.org/protobuf v1.31.0
-	gopkg.in/yaml.v2 v2.4.0
 )
 
 require (
 	github.com/go-kratos/aegis v0.2.0 // indirect
 	github.com/go-playground/form/v4 v4.2.0 // indirect
-	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/kr/text v0.2.0 // indirect
