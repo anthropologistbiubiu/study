e58fea446d79ca3b8e9b6cae83944be9cb2dff9d
anthropologistbiubiu <1319847957@qq.com>
2023-10-05 18:51:41 +0800

    modify

--- algorithm/day72/day72.go
+++ algorithm/day72/day72.go
@@ -6,5 +6,16 @@ func main() {
 
 // 编辑距离
 func minDistance(word1 string, word2 string) int {
+
+	if len(word1) <= 0 && len(word2) <= 0 {
+		return 0
+	} else if len(word1) == 0 {
+		return len(word2)
+	} else if len(word1) == 0 {
+		return len(word1)
+	}
 	return 0
 }
+
+
+
