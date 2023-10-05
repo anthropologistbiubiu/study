9e92c230a19027394a0cc97c95505c1f9a1dbca2
anthropologistbiubiu <1319847957@qq.com>
2023-10-06 01:42:15 +0800

    modify

--- algorithm/day72/day72.go
+++ algorithm/day72/day72.go
@@ -4,7 +4,13 @@ import "fmt"
 
 func main() {
 
-	fmt.Println(minDistance("horse", "ros"))
+	//fmt.Println(minDistance("horse", "ros"))
+	//"intention", word2 = "execution"
+	//fmt.Println(minDistance("intention", "execution"))
+	//fmt.Println(minDistance("a", "a"))
+	// "a"   "ab"
+	//fmt.Println(minDistance("a", "ab"))
+	fmt.Println(minDistance("abb", "a"))
 }
 
 /*
@@ -28,23 +34,32 @@ func min(j, k, l int) int {
 	return min
 }
 func minDistance(word1 string, word2 string) int {
-	m, n := len(word1), len(word2)
+	m, n := len(word1)+1, len(word2)+1
 	if m <= 0 && n <= 0 {
 		return 0
 	} else if m == 0 {
-		return n
+		return n - 1
 	} else if n == 0 {
-		return m
+		return m - 1
 	}
 	var dp = make([][]int, m)
-	for i := 0; i < len(word1); i++ {
+	for i := 0; i < m; i++ {
 		dp[i] = make([]int, n)
 	}
-	for i := 0; i < len(word1); i++ {
-		for j := 0; j < len(word2); j++ {
-			if word1[i] == word2[j] {
-				dp[i][j] = dp[i-1][j-1] + 1
+	for j := 1; j < n; j++ {
+		dp[0][j] = j
+	}
+	for i := 1; i < m; i++ {
+		dp[i][0] = i
+	}
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			if word1[i-1] == word2[j-1] {
+				dp[i][j] = dp[i-1][j-1]
 			} else {
+				// 插入 dp[i][j] = dp[i][j-1] + 1
+				// 删除 dp[i][j] = dp[i-1][j] + 1
+				// 修改 dp[i][j] = dp[i-1][j-1] + 1
 				dp[i][j] = min(dp[i-1][j]+1, dp[i][j-1]+1, dp[i-1][j-1]+1)
 			}
 		}
