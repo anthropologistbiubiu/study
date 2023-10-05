818ba15544842af4e40230475115961baaa1af47
anthropologistbiubiu <1319847957@qq.com>
2023-10-05 20:03:31 +0800

    modify

--- algorithm/day72/day72.go
+++ algorithm/day72/day72.go
@@ -1,10 +1,32 @@
 package main
 
+import "fmt"
+
 func main() {
 
+	fmt.Println(minDistance("horse", "ros"))
 }
 
+/*
+输入：word1 = "horse", word2 = "ros"
+输出：3
+解释：
+horse -> rorse (将 'h' 替换为 'r')
+rorse -> rose (删除 'r')
+rose -> ros (删除 'e')
+*/
 // 编辑距离
+
+func min(j, k, l int) int {
+	min := j
+	if k <= min {
+		min = k
+	}
+	if l <= min {
+		min = l
+	}
+	return min
+}
 func minDistance(word1 string, word2 string) int {
 	m, n := len(word1), len(word2)
 	if m <= 0 && n <= 0 {
@@ -20,7 +42,11 @@ func minDistance(word1 string, word2 string) int {
 	}
 	for i := 0; i < len(word1); i++ {
 		for j := 0; j < len(word2); j++ {
-
+			if word1[i] == word2[j] {
+				dp[i][j] = dp[i-1][j-1] + 1
+			} else {
+				dp[i][j] = min(dp[i-1][j]+1, dp[i][j-1]+1, dp[i-1][j-1]+1)
+			}
 		}
 	}
 	return dp[m-1][n-1]
