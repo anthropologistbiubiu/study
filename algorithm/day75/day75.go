404ff043e00231d9a74f8ffcd33878eea8ab81d6
anthropologistbiubiu <1319847957@qq.com>
2023-10-09 23:44:40 +0800

    modify

--- algorithm/day75/day75.go
+++ algorithm/day75/day75.go
@@ -1,10 +1,19 @@
 package day75
 
+// 给定一个包含非负整数的 m x n 网格 grid ，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
+// 说明：每次只能向下或者向右移动一步。
 func minPathSum(grid [][]int) int {
 
+	var dp = make([][]int, len(grid))
+	for i := 0; i < len(dp); i++ {
+
+	}
 	return 0
 }
 
 func main() {
 
 }
+
+
+
