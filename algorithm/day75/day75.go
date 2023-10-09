1fd7cbfce6051de8d9e659bbccd9e4b70fecc010
anthropologistbiubiu <1319847957@qq.com>
2023-10-09 23:48:52 +0800

    modify

--- algorithm/day75/day75.go
+++ algorithm/day75/day75.go
@@ -8,6 +8,13 @@ func minPathSum(grid [][]int) int {
 	for i := 0; i < len(dp); i++ {
 		dp[i] = make([]int, len(grid[0]))
 	}
+	dp[0][0] = grid[0][0]
+	for j := 1; j < len(grid[0]); j++ {
+		dp[0][j] = dp[0][j-1] + grid[0][j]
+	}
+	for i := 1; i < len(grid); i++ {
+		dp[i][0] = dp[i-1][0] + grid[i][0]
+	}
 	return 0
 }
 
