185d162a0521c8c955112cf57c8b9fca4843aa71
anthropologistbiubiu <1319847957@qq.com>
2023-10-11 00:05:58 +0800

    modify

--- algorithm/day77/day77.go
+++ algorithm/day77/day77.go
@@ -11,6 +11,7 @@ import "fmt"
 */
 func minimumTotal(triangle [][]int) int {
 	m := len(triangle)
+	var result int
 	var dp = make([][]int, m)
 	for i := 0; i < m; i++ {
 		dp[i] = make([]int, m)
@@ -19,15 +20,23 @@ func minimumTotal(triangle [][]int) int {
 	for i := 1; i < m; i++ {
 		dp[i][0] = dp[i-1][0] + triangle[i][0]
 	}
-	for i := 1; i < m; i++ {
-		dp[i][0] = dp[i-1][0] + triangle[i][0]
+	for j := 1; j < m; j++ {
+		var i = j
+		dp[i][j] = dp[i-1][j-1] + triangle[i][i]
 	}
-	for i := 2; i < m-1; i++ {
+	fmt.Println("dp", dp)
+	for i := 2; i < m; i++ {
 		for j := 1; j < i; j++ {
 			dp[i][j] = min(dp[i-1][j-1], dp[i-1][j]) + triangle[i][j]
 		}
 	}
-	return dp[m-1][m-1]
+	result = dp[m-1][0]
+	for j := 0; j < m; j++ {
+		if result > dp[m-1][j] {
+			result = dp[m-1][j]
+		}
+	}
+	return result
 }
 func min(a, b int) int {
 	if a < b {
