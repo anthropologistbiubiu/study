aa952c8771c7abe74c0e0df23af45e5f122f6735
anthropologistbiubiu <1319847957@qq.com>
2023-10-12 21:27:24 +0800

    modify

--- algorithm/day78/day78.go
+++ algorithm/day78/day78.go
@@ -4,6 +4,24 @@ import "fmt"
 
 func partition(s string) [][]string {
 
+	var length = len(s)
+	var dp = make([][]bool, length)
+	var ans = [][]string{}
+	for i := 0; i < length; i++ {
+		dp[i] = make([]bool, length)
+	}
+	for i := 0; i < length; i++ {
+		dp[i][i] = true
+	}
+	for i := 0; i < length; i++ {
+		for j := 0; j <= i; j++ {
+			if i == j {
+				dp[i][j] = true
+				st := s[i : j+1]
+
+			}
+		}
+	}
 	return [][]string{}
 }
 
---
+++ channel/day01/day01.go
@@ -0,0 +1,6 @@
+package main
+
+// 熟悉一下channel 的属性
+func main() {
+
+}
