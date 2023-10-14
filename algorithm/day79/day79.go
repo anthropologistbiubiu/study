5ef435eb36cc9978d86beb3305d9acda73230d44
anthropologistbiubiu <1319847957@qq.com>
2023-10-14 22:01:50 +0800

    modify

--- algorithm/day79/day79.go
+++ algorithm/day79/day79.go
@@ -1,12 +1,46 @@
-package day79
+package main
+
+import "fmt"
 
 func coinChange(coins []int, amount int) int {
 
 	var dp = make([]int, amount+1)
 	dp[0] = 0
 	for i := 1; i <= amount; i++ {
+		var min int
 		for _, v := range coins {
+			if i >= v && dp[i-v] > 0 || i-v == 0 {
+				if min >= dp[i-v]+1 || min == 0 {
+					min = dp[i-v] + 1
+				}
+			}
 		}
+		dp[i] = min
 	}
-	return 0
+	fmt.Println(dp)
+	if amount > 0 && dp[amount] == 0 {
+		return -1
+	}
+	return dp[amount]
+}
+
+func main() {
+	// 示例 1：
+	//
+	//输入：coins = [1, 2, 5], amount = 11
+	//输出：3
+	//解释：11 = 5 + 5 + 1
+	//fmt.Println(coinChange([]int{1, 2, 5}, 11))
+	//示例 2：
+	//
+	//fmt.Println(coinChange([]int{2}, 3))
+	//输入：coins = [2], amount = 3
+	//输出：-1
+	//示例 3：
+	//
+	fmt.Println(coinChange([]int{2, 5, 10, 1}, 27))
+	//输入：coins = [1], amount = 0
+	//输出：0
+	// 我应该尝试写一个单元测试函数，用来提高代码的质量
+	//[2,5,10,1]
 }
