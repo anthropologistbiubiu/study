dd0d74daa91c64fc16875393ade81c30e4e044f8
anthropologistbiubiu <1319847957@qq.com>
2023-10-16 21:25:45 +0800

    modify

--- gateway/conf/config_load.go
+++ gateway/conf/config_load.go
@@ -20,11 +20,18 @@ func LoadConfig() {
 	}
 	// 解析 YAML 配置并序列化到配置结构体
 	//fmt.Println("string string(configData", string(configData))
-	if err := yaml.Unmarshal(configData, server); err != nil {
+	var configs = map[string]interface{}{}
+	if err := yaml.Unmarshal(configData, configs); err != nil {
 		fmt.Println("UUU", err)
 	}
+	serverKV, _ := configs["server"].(map[interface{}]interface{})
+	//elasticsearchKV, _ := configs["elasticsearch"].(map[string]interface{})
+	Port, _ := serverKV["port"].(string)
+	fmt.Println("serverkv", serverKV, Port)
+	//server.Port = int32(Port)
+	server.Address, _ = serverKV["address"].(string)
 	// 现在我把这个配置怎么序列化位对应的结构体，使用的protobuf 协议
 	// 然后使用这个服务封装 es 和 mysql 的交互
 	fmt.Println("server.Address", server.Address)
-	fmt.Println("server.Address", server.Port)
+	fmt.Println("server.Port", server.Port)
 }
