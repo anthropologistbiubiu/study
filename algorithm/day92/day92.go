d135cba9d10ae49f813be2f39260057d3f4485e6
anthropologistbiubiu <1319847957@qq.com>
2023-10-17 23:50:53 +0800

    modify

--- algorithm/day92/day92.go
+++ algorithm/day92/day92.go
@@ -1,26 +1,25 @@
 package main
 
+import "fmt"
+
 // 给你一个整数数组 nums ，除某个元素仅出现 一次 外，其余每个元素都恰出现 三次 。请你找出并返回那个只出现了一次的元素。
-//
 // 你必须设计并实现线性时间复杂度的算法且使用常数级空间来解决此问题
-func main() {
-
-}
-
 func singleNumber(nums []int) int {
 
-	var dp = make([]int, len(nums))
+	var dp = make([]int, 32)
 	for _, v := range nums {
-		for i := 1; i <= 32; i++ {
-			dp[i] = (v >> i) & 1
+		for j := 0; j < 32; j++ {
+			dp[j] += (v >> j) & 1
 		}
 	}
+	fmt.Println("dp1", dp1)
 	var result int
 	for _, item := range dp {
 		if item%3 != 0 {
 			result = item
 		}
 	}
+	fmt.Println("dp", dp)
 	return result
 }
 
@@ -31,3 +30,22 @@ func singleNumber(nums []int) int {
 //示例 2：
 //输入：nums = [0,1,0,1,0,1,99]
 //输出：99
+
+func main() {
+	//fmt.Println(singleNumber([]int{2, 2, 3, 2}))
+	/*
+		var nums = []int{0, 1, 0, 1, 0}
+		var tem int = 1
+		var ans int
+		for i := len(nums) - 1; i >= 0; i-- {
+			if i == len(nums)-1 {
+				tem *= 1
+			} else {
+				tem *= 2
+			}
+			if nums[i]&1 == 1 {
+				ans += tem
+			}
+		}
+	*/
+}
