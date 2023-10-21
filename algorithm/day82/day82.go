4b1c3af0e4d8e0b3c6ac0047bd81a47dd10e63dd
anthropologistbiubiu <1319847957@qq.com>
2023-10-22 03:31:23 +0800

    modify

--- algorithm/day82/day82.go
+++ algorithm/day82/day82.go
@@ -1,13 +1,14 @@
 package main
 
-import "fmt"
-
 func main() {
 
-	var dp = make([]int, 32)
-	var b = -4
-	for i := 0; i < 32; i++ {
-		dp[i] = b >> i & 1
-	}
-	fmt.Println(dp)
+}
+
+//给你一个整数 n ，请你找出并返回第 n 个 丑数 。
+
+//说明：丑数是只包含质因数 2、3 和/或 5 的正整数；1 是丑数。
+
+func nthUglyNumber(n int) int {
+
+	return 0
 }
