0e1d33f9419345a5906da92108fe369703ec3278
anthropologistbiubiu <1319847957@qq.com>
2023-10-24 00:11:13 +0800

    modify

--- algorithm/day82/day82.go
+++ algorithm/day82/day82.go
@@ -4,12 +4,8 @@ import "fmt"
 
 func main() {
 
-	fmt.Println(nthUglyNumber(3))
+	fmt.Println(nthUglyNumber(10))
 }
-
-//给你一个整数 n ，请你找出并返回第 n 个 丑数 。
-
-//说明：丑数是只包含质因数 2、3 和/或 5 的正整数；1 是丑数。
 
 func nthUglyNumber(n int) int {
 
@@ -17,8 +13,10 @@ func nthUglyNumber(n int) int {
 	dp[0] = 1
 	var next int
 	var p2, p3, p5 = 0, 0, 0
+	var mp = map[int]struct{}{}
 	for i := 1; i < n; i++ {
-		dp[i], next = min(p2, p3, p5, dp)
+		mp[dp[i-1]] = struct{}{}
+		dp[i], next = min(p2, p3, p5, dp, mp)
 		if next == p2 {
 			p2++
 		} else if next == p3 {
@@ -27,19 +25,25 @@ func nthUglyNumber(n int) int {
 			p5++
 		}
 	}
+	fmt.Println(dp)
 	return dp[n-1]
 }
 
-func min(p2, p3, p5 int, dp []int) (int, int) {
+func min(p2, p3, p5 int, dp []int, mp map[int]struct{}) (int, int) {
+	fmt.Println("dp", dp, p2, p3, p5)
 	m := dp[p2] * 2
 	next := p2
 	if m > dp[p3]*3 {
-		m = dp[p3] * 3
-		next = p3
+		if _, ok := mp[dp[p3]*3]; !ok {
+			m = dp[p3] * 3
+			next = p3
+		}
 	}
 	if m > dp[p5]*5 {
-		m = dp[p5] * 5
-		next = p5
+		if _, ok := mp[dp[p5]*5]; !ok {
+			m = dp[p5] * 5
+			next = p5
+		}
 	}
 	return m, next
 }
