09c1bdd1064f83c99722aa79c95b51ea0b3820f0
anthropologistbiubiu <1319847957@qq.com>
2023-10-25 00:07:21 +0800

    modify

--- algorithm/day82/day82.go
+++ algorithm/day82/day82.go
@@ -4,20 +4,17 @@ import "fmt"
 
 func main() {
 
-	fmt.Println(nthUglyNumber(10))
+	fmt.Println(nthUglyNumber(11))
 }
 
 func nthUglyNumber(n int) int {
 
-	var dp = make([]int, 0)
+	var dp = make([]int, n)
 	dp[0] = 1
-	var next int
 	var p2, p3, p5 = 0, 0, 0
-	var mp = map[int]struct{}{}
-	var count int = 1
-	for count <= n {
-		mp[dp[i-1]] = struct{}{}
-		dp[i], next = min(p2, p3, p5, dp, mp)
+	for i := 1; i < n; i++ {
+		ans, next := min(p2, p3, p5, dp)
+		dp[i] = ans
 		if next == p2 {
 			p2++
 		} else if next == p3 {
@@ -25,20 +22,24 @@ func nthUglyNumber(n int) int {
 		} else if next == p5 {
 			p5++
 		}
-		count++
+		if ans == dp[i-1] {
+			i--
+		}
 	}
-	fmt.Println(dp)
 	return dp[n-1]
 }
 
 func min(p2, p3, p5 int, dp []int) (int, int) {
+	fmt.Println("dp", dp)
 	ans := dp[p2] * 2
 	next := p2
 	if ans > dp[p3]*3 {
 		ans = dp[p3] * 3
 		next = p2
-	} else {
-
+	}
+	if ans > dp[p5]*p5 {
+		ans = dp[p5] * p5
+		next = p5
 	}
 	return ans, next
 }
