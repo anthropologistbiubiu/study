9c2e2e92f78833125b0ddb6703d1117549489dbd
anthropologistbiubiu <1319847957@qq.com>
2023-11-01 23:22:13 +0800

    modify

--- algorithm/day84/day84.go
+++ algorithm/day84/day84.go
@@ -9,19 +9,19 @@ func main() {
 
 	ticker := time.NewTicker(1 * time.Second)
 	go func() {
-		select {
-		case <-ticker.C:
-			fmt.Println("主协程结束")
-			return
-		default:
-			go func() {
-				fmt.Println("child start")
-				time.Sleep(time.Second * 4)
-				fmt.Println("child end")
-			}()
+		for {
+			select {
+			case <-ticker.C:
+				fmt.Println("主协程结束")
+				return
+			default:
+				go func() {
+					//fmt.Println("child start")
+					time.Sleep(time.Second * 4)
+					fmt.Println("child end")
+				}()
+			}
 		}
-
 	}()
-	time.Sleep(6 * time.Second)
-
+	time.Sleep(3 * time.Second)
 }
