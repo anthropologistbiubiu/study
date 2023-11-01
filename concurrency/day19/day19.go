413696cbf2cce61fe736cedac77191a088422b58
anthropologistbiubiu <1319847957@qq.com>
2023-11-01 23:40:51 +0800

    modify

--- concurrency/day19/day19.go
+++ concurrency/day19/day19.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+func main() {
+	// 1 在这里需要你写算法
+	// 2 要求每秒钟调用一次proc函数
+	// 3 要求程序不能退出
+	t := time.NewTicker(time.Second * 1)
+	for {
+		select {
+		case <-t.C:
+			go func() {
+				defer func() {
+					fmt.Println(recover())
+				}()
+				proc()
+			}()
+		}
+	}
+
+}
+
+func proc() {
+	panic("ok")
 }
