0de6781f4157e536366a914cb746c9dd54488715
anthropologistbiubiu <1319847957@qq.com>
2023-11-22 23:41:59 +0800

    m

--- concurrency/day20/cond.go
+++ concurrency/day20/cond.go
@@ -1,18 +1,31 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+)
+
 type Set map[string]interface{}
 
 func NewSet() Set {
 	return make(Set)
 }
 
-func (s Set) Read() {
-
+func (s Set) Read(reader string) {
+	for k, v := range s {
+		fmt.Println(reader, k, v)
+	}
 }
 
-func (s Set) Write() {
-
+func (s Set) Write(nameArr ...string) {
+	for _, name := range nameArr {
+		s[name] = struct{}{}
+	}
 }
+
 func main() {
+
+	set := NewSet()
+	cond := sync.NewCond(&sync.Mutex{})
 
 }
