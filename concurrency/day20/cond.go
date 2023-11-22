2e368584d104314351c4bc2661a152edc82b0aec
anthropologistbiubiu <1319847957@qq.com>
2023-11-22 23:55:08 +0800

    m

--- concurrency/day20/cond.go
+++ concurrency/day20/cond.go
@@ -2,30 +2,47 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"sync"
+	"time"
 )
 
 type Set map[string]interface{}
+
+var (
+	Done = false
+)
 
 func NewSet() Set {
 	return make(Set)
 }
 
-func (s Set) Read(reader string) {
+func (s Set) Read(c *sync.Cond, reader string) {
+	c.L.Lock()
+	defer c.L.Unlock()
+	c.Wait()
 	for k, v := range s {
 		fmt.Println(reader, k, v)
 	}
 }
 
-func (s Set) Write(nameArr ...string) {
+func (s Set) Write(c *sync.Cond, nameArr ...string) {
+	c.L.Lock()
+	log.Print("starting writing")
 	for _, name := range nameArr {
 		s[name] = struct{}{}
 	}
+	c.L.Unlock()
+	c.Broadcast()
 }
 
 func main() {
 
 	set := NewSet()
 	cond := sync.NewCond(&sync.Mutex{})
-
+	go set.Write(cond, []string{"zhangsan", "lisi"}...)
+	go set.Read(cond, "reader1")
+	go set.Read(cond, "reader2")
+	go set.Read(cond, "reader3")
+	time.Sleep(time.Second * 3)
 }
