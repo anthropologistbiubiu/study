725e152bedabc111b8013aee6c64eead0936859c
anthropologistbiubiu <1319847957@qq.com>
2023-11-26 15:22:19 +0800

    m

--- redis/pub_sub/day01.go
+++ redis/pub_sub/day01.go
@@ -1,95 +1,35 @@
 package main
 
 import (
-	"context"
-	"github.com/redis/go-redis/v9"
 	"log"
 	"os"
 	"os/signal"
-	"sync"
 	"syscall"
 	"time"
 )
 
-// 直接在这里写一个回调服务算了，通过发布与订阅模式来完成。
-// 写一个阻塞的服务来完成读消息队列的数据，不提供接口，但是要实现对服务的优雅退出和重启。不能丢失消费的数据。
-/*
+func write(names ...string) {
 
-根据之前的服务对kafka服务写一个优雅退出过程的服务。
-第一 实现对订单状态的数据库修改，第二对回调订单使用协程发送请求。第三，对修改状态后的订单标记offset.
-如果标记后从管道读取下一个数据那么并发也就是失去了意义。 第四 加上并发控制。
-
-验证demo1 :设计并发标记数据的时机。如果是全部处理完成，那么数据的处理就是在顺序处理了。
-
-测试如果每标记一个，才会读取下个数据的话，那么是否可实现并发？
-
-第一 实现证明数据的并发消费 在宕机的情况下是不可靠的，会丢失数据，针对并发消费最好只设定好标记时机。
-
-第二 并发消费数据，采取手动标记数据其实是顺序执行的。
-
-第三 针对并发数据的消费要合理设置分区，合理设置标记时机。
-
-第四 要解决的是在优雅退出下，并发请求的优雅处理和消费者的关闭。
-
-*/
-// 上面记录的是对消息队列使用存在的疑惑,我这里来一步一步解决,后期来搭建为docker版本的集群模式
-
-var (
-	messageQueue = new(MessageQueue)
-)
-
-type MessageQueue struct {
-	client *redis.Client
-}
-
-func init() {
-	messageQueue.client = redis.NewClient(&redis.Options{
-		Addr:     "127.0.0.1:6379",
-		Password: "",
-		DB:       0,
-	})
-}
-
-func (m *MessageQueue) read(ctx context.Context) <-chan *redis.Message {
-	pubsub := messageQueue.client.Subscribe(ctx, "my_channel")
-	_, err := pubsub.Receive(ctx)
-	if err != nil {
-		log.Println("Receive", err)
-	}
-	msgChan := pubsub.Channel()
-	return msgChan
-}
-func (m *MessageQueue) write(ctx context.Context, message string) error {
-	err := m.client.Publish(ctx, "my_channel", message).Err()
-	if err != nil {
-		log.Println("Publish", err)
-	}
-	return err
-}
-
-func Producer(queue *MessageQueue, messages []string, ctx context.Context) {
-	for _, msg := range messages {
-		queue.write(ctx, msg)
-		log.Printf("msg %s\n", msg)
-		time.Sleep(time.Second * 1)
+	for {
+		time.Sleep(1 * time.Second)
+		log.Println("write", time.Now().Second())
 	}
 }
 
-func Consumer(queue *MessageQueue, ctx context.Context) {
-	msgChan := queue.read(ctx)
-	for msg := range msgChan {
-		log.Printf("receiv msg %s \n", msg)
+func read() {
+	for {
+		time.Sleep(1 * time.Second)
+		log.Println("read", time.Now().Second())
 	}
 }
+
 func main() {
 
-	var wg, ctx = sync.WaitGroup{}, context.Background()
-	messages := []string{"nihao", "chilema?", "zaiganma"}
-	wg.Add(2)
-	go Consumer(messageQueue, ctx)
-	go Producer(messageQueue, messages, ctx)
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
-	<-signalChan
-	wg.Wait()
+	sigchan := make(chan os.Signal, 1)
+	go write()
+	go read()
+	signal.Notify(sigchan, syscall.SIGTERM, syscall.SIGINT)
+	<-sigchan
+	time.Sleep(5 * time.Second)
+
 }
---
+++ redis/pub_sub/day02.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"context"
+	"github.com/redis/go-redis/v9"
+	"log"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+)
+
+// 直接在这里写一个回调服务算了，通过发布与订阅模式来完成。
+// 写一个阻塞的服务来完成读消息队列的数据，不提供接口，但是要实现对服务的优雅退出和重启。不能丢失消费的数据。
+/*
+
+根据之前的服务对kafka服务写一个优雅退出过程的服务。
+第一 实现对订单状态的数据库修改，第二对回调订单使用协程发送请求。第三，对修改状态后的订单标记offset.
+如果标记后从管道读取下一个数据那么并发也就是失去了意义。 第四 加上并发控制。
+
+验证demo1 :设计并发标记数据的时机。如果是全部处理完成，那么数据的处理就是在顺序处理了。
+
+测试如果每标记一个，才会读取下个数据的话，那么是否可实现并发？
+
+第一 实现证明数据的并发消费 在宕机的情况下是不可靠的，会丢失数据，针对并发消费最好只设定好标记时机。
+
+第二 并发消费数据，采取手动标记数据其实是顺序执行的。
+
+第三 针对并发数据的消费要合理设置分区，合理设置标记时机。
+
+第四 要解决的是在优雅退出下，并发请求的优雅处理和消费者的关闭。
+
+*/
+// 上面记录的是对消息队列使用存在的疑惑,我这里来一步一步解决,后期来搭建为docker版本的集群模式
+
+var (
+	messageQueue = new(MessageQueue)
+	signalChan   = make(chan os.Signal, 1)
+	wg, ctx      = sync.WaitGroup{}, context.Background()
+)
+
+type MessageQueue struct {
+	client *redis.Client
+}
+
+func init() {
+	messageQueue.client = redis.NewClient(&redis.Options{
+		Addr:     "127.0.0.1:6379",
+		Password: "",
+		DB:       0,
+	})
+}
+
+func (m *MessageQueue) read(ctx context.Context) <-chan *redis.Message {
+	pubsub := messageQueue.client.Subscribe(ctx, "my_channel")
+	_, err := pubsub.Receive(ctx)
+	if err != nil {
+		log.Println("Receive", err)
+	}
+	msgChan := pubsub.Channel()
+	return msgChan
+}
+func (m *MessageQueue) write(ctx context.Context, message string) error {
+	err := m.client.Publish(ctx, "my_channel", message).Err()
+	if err != nil {
+		log.Println("Publish", err)
+	}
+	return err
+}
+
+func Producer(queue *MessageQueue, messages []string, ctx context.Context) {
+	defer wg.Done()
+	for _, msg := range messages {
+		queue.write(ctx, msg)
+		log.Printf("msg %s\n", msg)
+		time.Sleep(time.Second * 2)
+		// 2 每两秒钟写进去一条消息
+	}
+}
+
+func Consumer(queue *MessageQueue, ctx context.Context) {
+	defer wg.Done()
+	msgChan := queue.read(ctx)
+	for {
+		select {
+		case <-signalChan:
+			return
+		default:
+			msg := <-msgChan
+			log.Printf("Consumer msg:%s\n", msg)
+		}
+	}
+	log.Println("quit safely")
+}
+
+func cleanWork() {
+
+}
+func main() {
+
+	messages := []string{"nihao", "chilema", "zaiganma", "今天天气不错", "good idea", "831", "mayday"}
+	wg.Add(3)
+	go Consumer(messageQueue, ctx)
+	go Producer(messageQueue, messages, ctx)
+	go func() {
+		for {
+			signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+			time.Sleep(3 * time.Second)
+			//os.Exit(1)
+		}
+	}()
+	wg.Wait()
+	// 整体应该是这样的一个效果，信号量管道监听当前的信号，然后主程序一直在运行当中，执行协程中的程序，然后
+	// 一边监听信号量,当监听到信号量的时候，程序应该退出，退出的时候应该考虑并发的协程有没有被执行完毕。
+}
