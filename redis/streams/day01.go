c3016e5b87310eebb6b224a8f279b407f894ed2e
anthropologistbiubiu <1319847957@qq.com>
2023-11-27 21:58:56 +0800

    m

--- redis/streams/day01.go
+++ redis/streams/day01.go
@@ -1,5 +1,17 @@
 package main
 
+import "github.com/redis/go-redis/v9"
+
+// 研究一下stream 做消息队列 这个数据结构
+// 按照那种简易的写法，来完成这个过程。
+
+func NewRedisClient() *redis.Client {
+	return redis.NewClient(*redis.Options{
+		Addr:     "",
+		Password: "",
+		DB:       0,
+	})
+}
 func main() {
 
 }
