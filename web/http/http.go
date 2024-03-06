ca8d7fa6bf27a16620ef258987defccd1fa6a44d
anthropologistbiubiu <1319847957@qq.com>
2024-03-06 23:36:11 +0800

    web

--- web/http/http.go
+++ web/http/http.go
@@ -11,11 +11,15 @@ func main() {
 		Addr: ":9090",
 		//Handler: http.HandleFunc("http", httpRequest),
 	}
-	http.HandleFunc("/fetch", handleRequest)
+	http.HandleFunc("/fetch1", handleRequest)
 	server.ListenAndServe()
 }
 
 func handleRequest(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
 	log.Print(r.URL)
-	w.Write([]byte("hello world"))
+	w.Write([]byte("{\"name\":\"sunweiming\"}"))
 }
