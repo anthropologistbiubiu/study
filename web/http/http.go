2c59484394020a07de77b6a5754b8e5c83988e05
anthropologistbiubiu <1319847957@qq.com>
2024-03-11 22:36:59 +0800

    http

--- web/http/http.go
+++ web/http/http.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -11,7 +12,7 @@ func main() {
 		Addr: ":9090",
 		//Handler: http.HandleFunc("http", httpRequest),
 	}
-	http.HandleFunc("/fetch", handleRequest)
+	http.HandleFunc("/submit", handleRequest)
 	server.ListenAndServe()
 }
 
@@ -19,7 +20,12 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-
-	log.Print(r.URL)
+	err := r.ParseForm()
+	if err != nil {
+		log.Println("err", err.Error())
+	}
 	w.Write([]byte("{\"name\":\"sunweiming\"}"))
+	username := r.FormValue("username")
+	passwd := r.FormValue("password")
+	fmt.Println(username, passwd)
 }
