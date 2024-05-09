262245c5a8e6def66a4a23bd86098605717b68ae
anthropologistbiubiu <1319847957@qq.com>
2024-05-09 22:44:58 +0800

    study

--- algorithm/primer/primer.go
+++ algorithm/primer/primer.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import "fmt"
 
+func gen(ch chan<- int) {
+	for i := 2; ; i++ {
+		ch <- i
+	}
+}
+
+func sieve(input <-chan int, output chan<- int, prime int) {
+	for num := range input {
+		if num%prime != 0 {
+			output <- num
+		}
+	}
+}
+
+func main() {
+	ch := make(chan int)
+	go gen(ch)
+	for i := 0; i < 10; i++ {
+		prime := <-ch
+		fmt.Println(prime)
+		newch := make(chan int)
+		go sieve(ch, newch, prime)
+		ch = newch
+	}
 }
