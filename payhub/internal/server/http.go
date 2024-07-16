e08f37b2ab809499ca9ca8f88bb049e7fdab05ef
anthropologistbiubiu <1319847957@qq.com>
2024-07-17 01:34:05 +0800

    study

--- payhub/internal/server/http.go
+++ payhub/internal/server/http.go
@@ -1,20 +1,24 @@
 package server
 
 import (
+	"github.com/go-kratos/kratos/v2/middleware/auth/jwt"
+	jwt2 "github.com/golang-jwt/jwt/v5"
 	v1 "payhub/api/helloworld/v1"
 	"payhub/internal/conf"
 	"payhub/internal/service"
 
 	"github.com/go-kratos/kratos/v2/log"
-	"github.com/go-kratos/kratos/v2/middleware/recovery"
 	"github.com/go-kratos/kratos/v2/transport/http"
 )
 
 // NewHTTPServer new an HTTP server.
 func NewHTTPServer(c *conf.Server, pay *service.PaymentOrderService, logger log.Logger) *http.Server {
+	/// type Handler func(ctx context.Context, req interface{}) (interface{}, error)
 	var opts = []http.ServerOption{
 		http.Middleware(
-			recovery.Recovery(),
+			jwt.Server(func(token *jwt2.Token) (interface{}, error) {
+				return token, nil
+			}),
 		),
 	}
 	if c.Http.Network != "" {
