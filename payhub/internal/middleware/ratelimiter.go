f998494437ac72c0c69999a3ceee6df34a12d5db
anthropologistbiubiu <1319847957@qq.com>
2024-07-27 23:31:53 +0800

    study

--- payhub/internal/middleware/ratelimiter.go
+++ payhub/internal/middleware/ratelimiter.go
@@ -4,18 +4,24 @@ import (
 	"context"
 	"fmt"
 	"github.com/go-kratos/kratos/v2/middleware"
-	//"github.com/go-kratos/kratos/v2/middleware/ratelimit"
 	limiter "github.com/juju/ratelimit"
 )
 
 func RateLimitMiddleware() middleware.Middleware {
 	bucket := limiter.NewBucketWithRate(1, 1) // 每秒1个请求，最多积累5个
 	return func(handler middleware.Handler) middleware.Handler {
-		fmt.Println("||||||||||")
 		return func(ctx context.Context, req interface{}) (reply interface{}, err error) {
 			if bucket.TakeAvailable(1) < 1 {
 				return nil, fmt.Errorf("rate limit exceeded")
 			}
+			return handler(ctx, req)
+		}
+	}
+}
+
+func ApiAuthMiddleWare() middleware.Middleware {
+	return func(handler middleware.Handler) middleware.Handler {
+		return func(ctx context.Context, req interface{}) (interface{}, error) {
 			return handler(ctx, req)
 		}
 	}
