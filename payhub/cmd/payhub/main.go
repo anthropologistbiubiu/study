1d40a468ada7b48283809fff9876a74ab1a5b522
anthropologistbiubiu <1319847957@qq.com>
2024-08-03 17:17:38 +0800

    study

--- payhub/cmd/payhub/main.go
+++ payhub/cmd/payhub/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"flag"
 	"github.com/go-kratos/kratos/v2"
-	"github.com/hashicorp/consul/api"
+	"github.com/prometheus/client_golang/prometheus"
 	"os"
 
 	"github.com/go-kratos/kratos/v2/config"
@@ -19,7 +19,7 @@ import (
 // go build -ldflags "-X main.Version=x.y.z"
 var (
 	// Name is the name of the compiled software.
-	Name string
+	//Name string
 	// Version is the version of the compiled software.
 	Version string
 	// flagconf is the config flag.
@@ -28,27 +28,52 @@ var (
 	id, _ = os.Hostname()
 )
 
+var (
+	// Name is the name of the compiled software.
+	Name = "metrics"
+	// Version is the version of the compiled software.
+	// Version = "v1.0.0"
+
+	_metricSeconds = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Namespace: "server",
+		Subsystem: "requests",
+		Name:      "duration_sec",
+		Help:      "server requests duration(sec).",
+		Buckets:   []float64{0.005, 0.01, 0.025, 0.05, 0.1, 0.250, 0.5, 1},
+	}, []string{"kind", "operation"})
+
+	_metricRequests = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Namespace: "client",
+		Subsystem: "requests",
+		Name:      "code_total",
+		Help:      "The total number of processed requests",
+	}, []string{"kind", "operation", "code", "reason"})
+)
+
 func init() {
 	flag.StringVar(&flagconf, "conf", "../../configs", "config path, eg: -conf config.yaml")
 }
 
 func newApp(logger log.Logger, gs *grpc.Server, hs *http.Server) *kratos.App {
 
-	consulConfig := api.DefaultConfig()
-	consulClient, err := api.NewClient(consulConfig)
-	if err != nil {
-		log.Fatalf("failed to create consul client: %v", err)
-	}
+	/*
+		consulConfig := api.DefaultConfig()
+		consulClient, err := api.NewClient(consulConfig)
+		if err != nil {
+			log.Fatalf("failed to create consul client: %v", err)
+		}
 
-	// 创建 Kratos 的 Consul 注册器
-	//r := consulRegistry.Registrar(context.Context(), consulClient)
+		// 创建 Kratos 的 Consul 注册器
+		//r := consulRegistry.Registrar(context.Context(), consulClient)
+
+	*/
 	return kratos.New(
 		kratos.ID(id),
 		kratos.Name(Name),
 		kratos.Version(Version),
 		kratos.Metadata(map[string]string{}),
 		kratos.Logger(logger),
-		kratos.Registrar(r),
+		//kratos.Registrar(r),
 		kratos.Server(
 			gs,
 			hs,
