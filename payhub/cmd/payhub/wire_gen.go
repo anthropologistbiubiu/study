05d0358f1e34f92918b96f23673014bf4a23cfeb
anthropologistbiubiu <1319847957@qq.com>
2024-08-17 23:19:42 +0800

    study

--- payhub/cmd/payhub/wire_gen.go
+++ payhub/cmd/payhub/wire_gen.go
@@ -32,8 +32,8 @@ func wireApp(confServer *conf.Server, confData *conf.Data, logger log.Logger) (*
 	paymentOrderUsecase := biz.NewPaymentOrderUsecase(paymentRepo, logger)
 	paymentOrderService := service.NewPaymentOrderService(paymentOrderUsecase)
 	grpcServer := server.NewGRPCServer(confServer, paymentOrderService, logger)
-	httpServer1 := server.NewHTTPServer(confServer, paymentOrderService, logger)
-	httpServer2 := server.NewHTTPServer(confServer, paymentOrderService, logger)
+	httpServer1 := server.NewHTTPServer1(confServer, paymentOrderService, logger)
+	httpServer2 := server.NewHTTPServer2(confServer, paymentOrderService, logger)
 	app := newApp(logger, grpcServer, httpServer1,httpServer2)
 	return app, func() {
 		cleanup()
