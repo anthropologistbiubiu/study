c69093a45be9349e1bc49cb528aabc172cd33ec8
anthropologistbiubiu <1319847957@qq.com>
2024-09-02 00:01:07 +0800

    study

--- payhub/go.mod
+++ payhub/go.mod
@@ -8,8 +8,11 @@ require (
 	github.com/google/wire v0.6.0
 	github.com/hashicorp/consul/api v1.29.2
 	github.com/juju/ratelimit v1.0.2
-	github.com/prometheus/client_golang v1.19.1
+	github.com/prometheus/client_golang v1.20.1
 	github.com/redis/go-redis/v9 v9.6.1
+	go.opentelemetry.io/otel/exporters/prometheus v0.51.0
+	go.opentelemetry.io/otel/metric v1.29.0
+	go.opentelemetry.io/otel/sdk/metric v1.29.0
 	go.uber.org/automaxprocs v1.5.3
 	google.golang.org/genproto/googleapis/api v0.0.0-20240812133136-8ffd90a71988
 	google.golang.org/grpc v1.65.0
@@ -26,7 +29,7 @@ require (
 	github.com/fatih/color v1.16.0 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/go-kratos/aegis v0.2.0 // indirect
-	github.com/go-logr/logr v1.4.1 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-playground/form/v4 v4.2.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
@@ -41,22 +44,22 @@ require (
 	github.com/hashicorp/serf v0.10.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/klauspost/compress v1.17.9 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
-	github.com/prometheus/client_model v0.5.0 // indirect
-	github.com/prometheus/common v0.48.0 // indirect
-	github.com/prometheus/procfs v0.12.0 // indirect
-	go.opentelemetry.io/otel v1.24.0 // indirect
-	go.opentelemetry.io/otel/metric v1.24.0 // indirect
-	go.opentelemetry.io/otel/sdk v1.24.0 // indirect
-	go.opentelemetry.io/otel/sdk/metric v1.24.0 // indirect
-	go.opentelemetry.io/otel/trace v1.24.0 // indirect
+	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/prometheus/client_model v0.6.1 // indirect
+	github.com/prometheus/common v0.55.0 // indirect
+	github.com/prometheus/procfs v0.15.1 // indirect
+	go.opentelemetry.io/otel v1.29.0 // indirect
+	go.opentelemetry.io/otel/sdk v1.29.0 // indirect
+	go.opentelemetry.io/otel/trace v1.29.0 // indirect
 	golang.org/x/exp v0.0.0-20230817173708-d852ddb80c63 // indirect
 	golang.org/x/net v0.26.0 // indirect
 	golang.org/x/sync v0.7.0 // indirect
-	golang.org/x/sys v0.21.0 // indirect
+	golang.org/x/sys v0.24.0 // indirect
 	golang.org/x/text v0.16.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240730163845-b1a4ccb954bf // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- payhub/internal/server/http.go
+++ payhub/internal/server/http.go
@@ -1,40 +1,36 @@
 package server
 
 import (
-	//prom "github.com/go-kratos/kratos/contrib/metrics/prometheus/v2"
-	"github.com/go-kratos/kratos/v2/middleware/metrics"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
-	"go.opentelemetry.io/otel"
-	"payhub/api/v1"
-
+	_ "context"
 	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/middleware/metrics"
 	"github.com/go-kratos/kratos/v2/transport/http"
 	"github.com/prometheus/client_golang/prometheus"
-
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"go.opentelemetry.io/otel"
+	prom "go.opentelemetry.io/otel/exporters/prometheus"
+	_ "go.opentelemetry.io/otel/metric"
+	"go.opentelemetry.io/otel/sdk/metric"
+	"payhub/api/v1"
 	"payhub/internal/conf"
 	"payhub/internal/middleware"
 	"payhub/internal/service"
 )
 
-func init() {
-	//prometheus.MustRegister(_metricSeconds, _metricRequests)
-}
+// NewPrometheusCounter creates a new PrometheusCounter adapter.
 
 var (
 	// Name is the name of the compiled software.
 	Name = "metrics"
 	// Version is the version of the compiled software.
 	// Version = "v1.0.0"
-	_metricRequests, _ = otel.Meter("payhub_meter").Int64Counter(metrics.DefaultServerRequestsCounterName)
-	_metricSeconds, _  = otel.Meter("payhub_meter").Float64Histogram(metrics.DefaultServerSecondsHistogramName)
-	metricSeconds      = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+	metricSeconds = prometheus.NewHistogramVec(prometheus.HistogramOpts{
 		Namespace: "server",
 		Subsystem: "requests",
 		Name:      "duration_sec",
 		Help:      "server requests duration(sec).",
 		Buckets:   []float64{0.005, 0.01, 0.025, 0.05, 0.1, 0.250, 0.5, 1},
 	}, []string{"kind", "operation"})
-
 	metricRequests = prometheus.NewCounterVec(prometheus.CounterOpts{
 		Namespace: "client",
 		Subsystem: "requests",
@@ -45,8 +41,23 @@ var (
 
 // NewHTTPServer new an HTTP server.
 func NewHTTPServer1(c *conf.Server, pay *service.PaymentOrderService, logger log.Logger) *http.Server {
-	/// type Handler func(ctx context.Context, req interface{}) (interface{}, error)
-	//mylimiter := bbr.NewLimiter(bbr.WithWindow(1*time.Hour), bbr.WithBucket(1))
+	promExporter, err := prom.New()
+	if err != nil {
+		log.Fatalf("failed to initialize Prometheus exporter: %v", err)
+	}
+	meterProvider := metric.NewMeterProvider(metric.WithReader(promExporter))
+	otel.SetMeterProvider(meterProvider)
+	//meter := meterProvider.Meter("my-service-meter")
+	meter := otel.Meter("my-payhub1-meter")
+	if err != nil {
+		log.Fatalf("failed to create Int64Counter: %v", err)
+	}
+	requestsCounter, err := meter.Int64Counter(
+		"payhub1_http_requests_total",
+	)
+	if err != nil {
+		log.Fatalf("failed to create Int64Counter: %v", err)
+	}
 	var opts = []http.ServerOption{
 		http.Middleware(
 			/*
@@ -59,9 +70,7 @@ func NewHTTPServer1(c *conf.Server, pay *service.PaymentOrderService, logger log
 			middleware.RateLimitMiddleware1(),
 			middleware.AccessLogMiddleware(logger),
 			metrics.Server(
-				metrics.WithRequests(_metricRequests),
-				metrics.WithSeconds(_metricSeconds),
-			),
+				metrics.WithRequests(requestsCounter)),
 		),
 	}
 	if c.Http1.Network != "" {
@@ -95,10 +104,6 @@ func NewHTTPServer2(c *conf.Server, pay *service.PaymentOrderService, logger log
 			//middleware.IpWhiteMiddleware(middleware.WhiteList),
 			middleware.RateLimitMiddleware2(),
 			middleware.AccessLogMiddleware(logger),
-			metrics.Server(
-				metrics.WithRequests(_metricRequests),
-				metrics.WithSeconds(_metricSeconds),
-			),
 		),
 	}
 	if c.Http2.Network != "" {
