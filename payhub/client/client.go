2ea3873334e55bb070b6a4f5b518383f31d3a515
anthropologistbiubiu <1319847957@qq.com>
2024-10-05 15:03:22 +0800

    study

--- payhub/client/client.go
+++ payhub/client/client.go
@@ -3,15 +3,44 @@ package main
 import (
 	"context"
 	"fmt"
+	kratosCircuitbreaker "github.com/go-kratos/aegis/circuitbreaker"
+	"github.com/go-kratos/aegis/circuitbreaker/sre"
 	"github.com/go-kratos/kratos/contrib/registry/consul/v2"
+	"github.com/go-kratos/kratos/v2/middleware/circuitbreaker"
 	"github.com/go-kratos/kratos/v2/selector"
 	"github.com/go-kratos/kratos/v2/selector/filter"
 	"github.com/go-kratos/kratos/v2/selector/wrr"
 	kratos_http "github.com/go-kratos/kratos/v2/transport/http"
 	"github.com/hashicorp/consul/api"
+	"log"
 	"payhub/api/v1"
+	"time"
 )
 
+type loggingBreaker struct {
+	kratosCircuitbreaker.CircuitBreaker
+	name string
+}
+
+func (lb *loggingBreaker) Allow() error {
+	err := lb.CircuitBreaker.Allow()
+	if err != nil {
+		log.Printf("Circuit breaker '%s' is open, request blocked\n", lb.name)
+	} else {
+		log.Printf("Circuit breaker '%s' is closed, request allowed\n", lb.name)
+	}
+	return err
+}
+
+func (lb *loggingBreaker) MarkSuccess() {
+	lb.CircuitBreaker.MarkSuccess()
+	log.Printf("Circuit breaker '%s': request succeeded\n", lb.name)
+}
+
+func (lb *loggingBreaker) MarkFailed() {
+	lb.CircuitBreaker.MarkFailed()
+	log.Printf("Circuit breaker '%s': request failed\n", lb.name)
+}
 func main() {
 	// 创建 Consul 客户端
 	consulClient, err := api.NewClient(api.DefaultConfig())
@@ -24,13 +53,33 @@ func main() {
 	versionFilter := filter.Version("1.0.0")
 	// 设置全局的 Selector，使用 wrr 算法
 	selector.SetGlobalSelector(wrr.NewBuilder())
+	// 配置熔断器的参数
+	// 定义生成熔断器的函数
+	genBreakerFunc := func() kratosCircuitbreaker.CircuitBreaker {
+		// 创建一个 SRE 熔断器，可以自定义参数
+		breaker := sre.NewBreaker(
+			sre.WithWindow(time.Second*10),
+			sre.WithBucket(1),
+			sre.WithSuccess(2),
+		)
+		return &loggingBreaker{
+			CircuitBreaker: breaker,
+			name:           "my-circuit-breaker",
+		}
+	}
 	// 创建 HTTP 客户端
 	hConn, err := kratos_http.NewClient(
 		context.Background(),
-		kratos_http.WithEndpoint("discovery:///payhub"),
+		//kratos_http.WithEndpoint("discovery:///payhub"),
 		kratos_http.WithDiscovery(r),
 		kratos_http.WithNodeFilter(versionFilter),
 		kratos_http.WithBlock(),
+		kratos_http.WithMiddleware(
+			circuitbreaker.Client(
+				circuitbreaker.WithCircuitBreaker(genBreakerFunc),
+			),
+		),
+		kratos_http.WithEndpoint("127.0.0.1:8005"),
 	)
 	if err != nil {
 		panic(err)
